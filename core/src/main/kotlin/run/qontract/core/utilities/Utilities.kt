d53cd3dd98234a36e1b7ceae7868166d0fc5fb6a
Joel.Rosario <cirquitz@gmail.com>
2020-04-23 23:09:12 +0530

    - Moved the slf4j-nop reference into the application - Excluded the bouncycastle classes for now

--- core/src/main/kotlin/run/qontract/core/utilities/Utilities.kt
+++ core/src/main/kotlin/run/qontract/core/utilities/Utilities.kt
@@ -2,49 +2,17 @@
 
 package run.qontract.core.utilities
 
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.request.accept
-import io.ktor.client.request.request
-import io.ktor.client.statement.HttpResponse
-import io.ktor.client.statement.readText
-import io.ktor.http.HttpStatusCode
-import io.ktor.util.KtorExperimentalAPI
-import kotlinx.coroutines.runBlocking
 import org.w3c.dom.Document
 import org.w3c.dom.Node
 import org.xml.sax.InputSource
 import org.xml.sax.SAXException
 import java.io.*
-import java.net.URL
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.parsers.ParserConfigurationException
 import javax.xml.transform.OutputKeys
 import javax.xml.transform.TransformerFactory
 import javax.xml.transform.dom.DOMSource
 import javax.xml.transform.stream.StreamResult
-
-@OptIn(KtorExperimentalAPI::class)
-fun readFromAPI(urlStr: String?): String {
-    val ktorClient = io.ktor.client.HttpClient(CIO)
-
-    var response = ""
-
-    val url = URL(urlStr)
-
-    runBlocking {
-        val ktorResponse: HttpResponse = ktorClient.request(url) {
-            this.method = io.ktor.http.HttpMethod.Get
-            this.accept(io.ktor.http.ContentType.Application.Json)
-        }
-
-        if(ktorResponse.status != HttpStatusCode.OK)
-            throw Exception("API responded with ${ktorResponse.status}")
-
-        response = ktorResponse.readText()
-    }
-
-    return response
-}
 
 @Throws(IOException::class)
 fun readFile(filePath: String): String {
@@ -81,9 +49,7 @@ private fun xmlToString(domSource: DOMSource): String {
 
 val contractFilePath: String
     get() = currentDirectory + defaultContractFilePath
-var brokerURL: String = "http://localhost:8089"
+
 private const val currentDirectory = "./"
 private const val contractDirectory = "contract"
 private const val defaultContractFilePath = "$contractDirectory/service.contract"
-private const val serviceManifestFilename = "component.yaml"
-const val serviceManifestPath: String = currentDirectory + serviceManifestFilename

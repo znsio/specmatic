c8191cc3158367248d16a8eb52cdf81099bb2298
Joel.Rosario <cirquitz@gmail.com>
2020-04-29 18:30:37 +0530

    Backward compatibility results in an exception

--- core/src/main/kotlin/run/qontract/core/ContractUtilities.kt
+++ core/src/main/kotlin/run/qontract/core/ContractUtilities.kt
@@ -1,6 +1,7 @@
 @file:JvmName("ContractUtilities")
 package run.qontract.core
 
+import run.qontract.core.pattern.ContractException
 import java.io.File
 
 fun getContractFileName(directory: String, majorVersion: Int, minorVersion: Int? = null): String? {
@@ -8,6 +9,15 @@ fun getContractFileName(directory: String, majorVersion: Int, minorVersion: Int?
         is JustOne -> File(result.file).absolutePath
         is NoContractsFound -> return null
         is TestResults -> {
+            if(minorVersion != null && result.list.any { !it.results.success() } ) {
+                val firstFailure = result.list.first { !it.results.success() }
+
+                throw ContractException("""Backward compatibility breakage detected.
+${firstFailure.older} => ${firstFailure.newer}
+${firstFailure.results.report()}
+""".trim())
+            }
+
             File(if(!result.list.first().results.success())
                 result.list.first().older
             else

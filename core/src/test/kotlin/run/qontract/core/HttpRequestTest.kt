d5b5cc878ae580db4bd3ff8f438d956d77c5075e
Joel.Rosario <cirquitz@gmail.com>
2020-05-01 01:52:57 +0530

    Added tests to make sure that parsed patterns are handled correctly in mocked requests

--- core/src/test/kotlin/run/qontract/core/HttpRequestTest.kt
+++ core/src/test/kotlin/run/qontract/core/HttpRequestTest.kt
@@ -2,7 +2,8 @@ package run.qontract.core
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import run.qontract.core.pattern.parsedValue
+import org.junit.jupiter.api.fail
+import run.qontract.core.pattern.*
 import run.qontract.core.value.EmptyString
 import run.qontract.core.value.JSONObjectValue
 import run.qontract.core.value.StringValue
@@ -37,5 +38,46 @@ internal class HttpRequestTest {
     fun `request with a nullable string should result in an Any null or string pattern`() {
         val request = HttpRequest("POST", "/", emptyMap(), parsedValue("""{"data": "(string?)"}"""))
         val requestPattern = request.toPattern()
+        val body = requestPattern.body
+        if(body !is JSONObjectPattern) fail("Expected json object pattern")
+
+        val dataPattern  = body.pattern.getValue("data") as DeferredPattern
+        val nullableStringPattern = AnyPattern(listOf(NullPattern, StringPattern))
+
+        assertThat(dataPattern.resolvePattern(Resolver())).isEqualTo(nullableStringPattern)
+    }
+
+    @Test
+    fun `request with a string star should result in a string list pattern`() {
+        val request = HttpRequest("POST", "/", emptyMap(), parsedValue("""{"data": "(string*)"}"""))
+        val requestPattern = request.toPattern()
+        val body = requestPattern.body
+        if(body !is JSONObjectPattern) fail("Expected json object pattern")
+
+        val dataPattern  = body.pattern.getValue("data") as DeferredPattern
+        val listOfStringsPattern = ListPattern(StringPattern)
+
+        assertThat(dataPattern.resolvePattern(Resolver())).isEqualTo(listOfStringsPattern)
+    }
+
+    @Test
+    fun `request with a string rest should result in a string rest pattern`() {
+        val request = HttpRequest("POST", "/", emptyMap(), parsedValue("""{"data": ["(string...)"]}"""))
+        val requestPattern = request.toPattern()
+        val body = requestPattern.body
+
+        if(body !is JSONObjectPattern) fail("Expected json object pattern")
+
+        val dataPattern  = body.pattern.getValue("data") as JSONArrayPattern
+        val deferredPattern = dataPattern.pattern.first() as DeferredPattern
+        assertThat(deferredPattern.resolvePattern(Resolver())).isEqualTo(RestPattern(StringPattern))
+    }
+
+    @Test
+    fun `when request body is string question it should result in nullable string pattern as body`() {
+        val request = HttpRequest("POST", "/", emptyMap(), parsedValue("(string?)"))
+        val requestPattern = request.toPattern()
+        val deferredBodyPattern = requestPattern.body as DeferredPattern
+        assertThat(deferredBodyPattern.resolvePattern(Resolver())).isEqualTo(AnyPattern(listOf(NullPattern, StringPattern)))
     }
 }

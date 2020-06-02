bdf6b50f643e636c0441659eb5906c79a5350f47
Joel.Rosario <cirquitz@gmail.com>
2020-06-02 17:39:07 +0530

    Added tests for the sample command

--- application/src/main/kotlin/application/SamplesCommand.kt
+++ application/src/main/kotlin/application/SamplesCommand.kt
@@ -3,16 +3,16 @@ package application
 import picocli.CommandLine.*
 import run.qontract.core.Contract
 import run.qontract.core.pattern.ContractException
-import run.qontract.core.utilities.readFile
 import run.qontract.stub.HttpStub
+import java.io.File
 import java.util.concurrent.Callable
 
 @Command(name = "samples",
         mixinStandardHelpOptions = true,
         description = ["Generate samples of the API requests and responses for all scenarios"])
-class SamplesCommand : Callable<Void> {
+class SamplesCommand : Callable<Unit> {
     @Parameters(index = "0", description = ["Contract file path"])
-    var contractPath: String = ""
+    lateinit var qontractFile: File
 
     @Option(names = ["--host"], description = ["The host to bind to, e.g. localhost or some locally bound IP"], defaultValue = "localhost")
     var host: String = "127.0.0.1"
@@ -20,13 +20,9 @@ class SamplesCommand : Callable<Void> {
     @Option(names = ["--port"], description = ["The port to bind to"], defaultValue = "9000")
     var port: Int = 9000
 
-    override fun call(): Void? {
+    override fun call() {
         try {
-            val gherkin = readFile(contractPath)
-
-            HttpStub(gherkin, emptyList(), host, port).use { fake ->
-                Contract(gherkin).samples(fake)
-            }
+            samples(qontractFile, host, port)
         }
         catch(e: ContractException) {
             println(e.message)
@@ -36,7 +32,13 @@ class SamplesCommand : Callable<Void> {
             val message = listOf("Exception", "Class=${exception.javaClass.name}".prependIndent(indent), exception.message?.prependIndent(indent)).joinToString("\n")
             println(message)
         }
+    }
+}
 
-        return null
+fun samples(gherkinFile: File, host: String, port: Int) {
+    val gherkin = gherkinFile.readText().trim()
+
+    HttpStub(gherkin, emptyList(), host, port).use { fake ->
+        Contract(gherkin).samples(fake)
     }
 }
---
+++ application/src/test/kotlin/application/SamplesCommandKtTest.kt
@@ -0,0 +1,83 @@
+package application
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.PrintStream
+
+internal class SamplesCommandKtTest {
+    @TempDir
+    lateinit var tempDir: File
+
+    val simpleGherkin = """Feature: Math API
+  Scenario: Square
+    When POST /square
+    And request-body (number)
+    Then status 200
+    And response-body (number)"""
+
+    lateinit var qontractFile: File
+
+    @Test
+    fun `samples function should generate sample given the input`() {
+        val (data, _) = captureStandardOutput {
+            samples(qontractFile, "localhost", 9000)
+        }
+
+        validateOutput(data.trimIndent())
+    }
+
+    @Test
+    fun `command should generate sample given the input`() {
+        val (data, _) = captureStandardOutput {
+            val command = SamplesCommand()
+            command.qontractFile = qontractFile
+
+            samples(qontractFile, "localhost", 9000)
+        }
+
+        validateOutput(data)
+    }
+
+    private fun validateOutput(data: String) {
+        assertThat(data).startsWith(""">> Request Start At """)
+
+        assertThat(data).contains("""-> POST /square
+-> Accept-Charset: UTF-8
+-> Accept: */*
+->""".trimIndent())
+
+        assertThat(data).contains("""<- 200 OK
+<- Vary: Origin
+<- X-Qontract-Result: success
+<- Content-Length: 3
+<- Content-Type: text/plain
+<- 
+""".trimIndent())
+    }
+
+    @BeforeEach
+    fun setup() {
+        qontractFile = File(tempDir, "math.qontract").also {
+            it.writeText(simpleGherkin)
+        }
+    }
+}
+
+fun <ReturnType> captureStandardOutput(fn: () -> ReturnType): Pair<String, ReturnType> {
+    val originalOut = System.out
+
+    val byteArrayOutputStream = ByteArrayOutputStream()
+    val newOut = PrintStream(byteArrayOutputStream)
+    System.setOut(newOut)
+
+    val result = fn()
+
+    System.out.flush()
+    System.setOut(originalOut) // So you can print again
+    return Pair(String(byteArrayOutputStream.toByteArray()).trim(), result)
+}

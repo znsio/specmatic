0befbbce43fd1b94b7954fbabf9bcda079373e0f
Joel Rosario <cirquitz@gmail.com>
2020-06-20 21:17:13 +0530

    Improved error messages when there are exceptions to show all message in the stack.

--- core/src/main/kotlin/run/qontract/core/utilities/Utilities.kt
+++ core/src/main/kotlin/run/qontract/core/utilities/Utilities.kt
@@ -5,27 +5,30 @@ package run.qontract.core.utilities
 import org.w3c.dom.Document
 import org.w3c.dom.Node
 import org.xml.sax.InputSource
-import org.xml.sax.SAXException
 import java.io.*
 import javax.xml.parsers.DocumentBuilderFactory
-import javax.xml.parsers.ParserConfigurationException
 import javax.xml.transform.OutputKeys
 import javax.xml.transform.TransformerFactory
 import javax.xml.transform.dom.DOMSource
 import javax.xml.transform.stream.StreamResult
 
-fun exceptionCauseMessage(e: Throwable): String =
-        when(val cause = e.cause) {
-            null -> e
-            else -> cause
-        }.let {
-            exceptionMessage(it)
+fun exceptionCauseMessage(e: Throwable): String {
+    return when(val messageStack = exceptionMessageStack(e, emptyList())) {
+        emptyList<String>() -> "Exception class=${e.javaClass.name}, no message found"
+        else -> {
+            val messageString = messageStack.joinToString("; ") { it.trim().removeSuffix(".") }
+            "Error: $messageString"
         }
+    }
+}
 
-fun exceptionMessage(e: Throwable): String {
-    return when (val message = e.localizedMessage ?: e.message) {
-        null -> "Exception class=${e.javaClass.name}, no message found"
-        else -> "Error: $message"
+fun exceptionMessageStack(e: Throwable, messages: List<String>): List<String> {
+    val message = e.localizedMessage ?: e.message
+    val newMessages = if(message != null) messages.plus(message) else messages
+
+    return when(val cause = e.cause) {
+        null -> newMessages
+        else -> exceptionMessageStack(cause, newMessages)
     }
 }
 

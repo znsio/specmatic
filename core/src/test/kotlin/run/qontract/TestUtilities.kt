9f2e759b343ccbb2c95df9fa49bb5e73d476be1d
Joel Rosario <cirquitz@gmail.com>
2020-08-10 20:28:53 +0530

    Added a few basic tests around backward compatibility

--- core/src/test/kotlin/run/qontract/TestUtilities.kt
+++ core/src/test/kotlin/run/qontract/TestUtilities.kt
@@ -1,11 +1,13 @@
 package run.qontract
 
-import run.qontract.core.Resolver
+import org.assertj.core.api.Assertions
+import run.qontract.core.*
 import run.qontract.core.pattern.AnyPattern
 import run.qontract.core.pattern.DeferredPattern
 import run.qontract.core.pattern.Pattern
-import run.qontract.core.resultReport
+import run.qontract.core.utilities.exceptionCauseMessage
 import run.qontract.core.value.Value
+import run.qontract.mock.ScenarioStub
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -22,3 +24,53 @@ infix fun Value.shouldNotMatch(pattern: Pattern) {
 }
 
 fun emptyPattern() = DeferredPattern("(empty)")
+
+infix fun String.backwardCompatibleWith(oldContractGherkin: String) {
+    val results = testBackwardCompatibility(oldContractGherkin)
+    Assertions.assertThat(results.success()).isTrue()
+    Assertions.assertThat(results.failureCount).isZero()
+}
+
+infix fun String.notBackwardCompatibleWith(oldContractGherkin: String) {
+    val results = testBackwardCompatibility(oldContractGherkin)
+    Assertions.assertThat(results.success()).isFalse()
+    Assertions.assertThat(results.failureCount).isPositive()
+}
+
+fun String.testBackwardCompatibility(oldContractGherkin: String): Results {
+    val oldFeature = Feature(oldContractGherkin)
+    val newFeature = Feature(this)
+    return testBackwardCompatibility(oldFeature, newFeature)
+}
+
+fun stubShouldNotBreak(stubRequest: HttpRequest, stubResponse: HttpResponse, oldContract: String, newContract: String) {
+    val responseFromOldContract = testStub(oldContract, stubRequest, stubResponse)
+    Assertions.assertThat(responseFromOldContract).isEqualTo(stubResponse)
+
+    val responseFromNewContract = testStub(newContract, stubRequest, stubResponse)
+    Assertions.assertThat(responseFromNewContract.status).isEqualTo(200)
+}
+
+fun stubShouldBreak(stubRequest: HttpRequest, stubResponse: HttpResponse, oldContract: String, newContract: String) {
+    val responseFromOldContract = testStub(oldContract, stubRequest, stubResponse)
+    Assertions.assertThat(responseFromOldContract).isEqualTo(stubResponse)
+
+    val responseFromNewContract = try {
+        testStub(newContract, stubRequest, stubResponse)
+    } catch(e: Throwable) {
+        println(exceptionCauseMessage(e))
+        return
+    }
+
+    Assertions.assertThat(responseFromNewContract.status).isEqualTo(400)
+}
+
+fun testStub(oldContract: String, stubRequest: HttpRequest, stubResponse: HttpResponse): HttpResponse {
+    val feature = Feature(oldContract)
+    val stub = ScenarioStub(stubRequest, stubResponse)
+    val matchingStub = feature.matchingStub(stub)
+
+    return run.qontract.stub.stubResponse(stubRequest, listOf(feature), listOf(matchingStub), true).let {
+        it.copy(headers = it.headers - "X-Qontract-Result")
+    }
+}
---
+++ core/src/test/kotlin/run/qontract/core/BackwardCompatibilityModel.kt
@@ -0,0 +1,219 @@
+package run.qontract.core
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import run.qontract.backwardCompatibleWith
+import run.qontract.core.pattern.parsedJSONStructure
+import run.qontract.notBackwardCompatibleWith
+import run.qontract.stubShouldBreak
+import run.qontract.stubShouldNotBreak
+
+class BackwardCompatibilityModel {
+    val oldContract = """
+Feature: User API
+  Scenario: Add user
+    Given json User
+    | name | (string) |
+    And json Status
+    | status | (string) |
+    When POST /user
+    And request-body (User)
+    Then status 200
+    And response-body (Status)
+""".trimIndent()
+
+    @Test
+    fun `add non-optional key in the request body`() {
+        val newContract = """
+Feature: User API
+  Scenario: Add user
+    Given json User
+    | name    | (string) |
+    | address | (string) |
+    And json Status
+    | status | (string) |
+    When POST /user
+    And request-body (User)
+    Then status 200
+    And response-body success
+    And response-body (Status)
+""".trimIndent()
+
+        newContract notBackwardCompatibleWith oldContract
+
+        val request = HttpRequest(
+                "POST",
+                "/user",
+                body = parsedJSONStructure("""{"name": "John Doe"}"""))
+        val response = HttpResponse.OK(parsedJSONStructure("""{"status": "success"}"""))
+        stubShouldBreak(request, response, oldContract, newContract)
+    }
+
+    @Test
+    fun `add optional key in the request body`() {
+        val newContract = """
+Feature: User API
+  Scenario: Add user
+    Given json User
+    | name     | (string) |
+    | address? | (string) |
+    And json Status
+    | status | (string) |
+    When POST /user
+    And request-body (User)
+    Then status 200
+    And response-body success
+    And response-body (Status)
+""".trimIndent()
+
+        newContract backwardCompatibleWith oldContract
+
+        val request = HttpRequest("POST","/user", body = parsedJSONStructure("""{"name": "John Doe"}"""))
+        val response = HttpResponse.OK(parsedJSONStructure("""{"status": "success"}"""))
+        stubShouldNotBreak(request, response, oldContract, newContract)
+    }
+
+    @Test
+    fun `make type optional in request body`() {
+        val newContract = """
+Feature: User API
+  Scenario: Add user
+    Given json User
+    | name     | (string?) |
+    And json Status
+    | status | (string) |
+    When POST /user
+    And request-body (User)
+    Then status 200
+    And response-body success
+    And response-body (Status)
+""".trimIndent()
+
+        newContract backwardCompatibleWith oldContract
+
+        val request = HttpRequest("POST","/user", body = parsedJSONStructure("""{"name": "John Doe"}"""))
+        val response = HttpResponse.OK(parsedJSONStructure("""{"status": "success"}"""))
+        stubShouldNotBreak(request, response, oldContract, newContract)
+    }
+
+    @Test
+    fun `change number in string to string in request body`() {
+        val oldContract = """
+Feature: User API
+  Scenario: Add user
+    Given json User
+    | id | (number in string) |
+    And json Status
+    | status | (string) |
+    When POST /user
+    And request-body (User)
+    Then status 200
+    And response-body success
+    And response-body (Status)
+        """.trimIndent()
+
+        val newContract = """
+Feature: User API
+  Scenario: Add user
+    Given json User
+    | id | (string) |
+    And json Status
+    | status | (string) |
+    When POST /user
+    And request-body (User)
+    Then status 200
+    And response-body success
+    And response-body (Status)
+""".trimIndent()
+
+        newContract backwardCompatibleWith oldContract
+
+        val request = HttpRequest("POST","/user", body = parsedJSONStructure("""{"id": "10"}"""))
+        val response = HttpResponse.OK(parsedJSONStructure("""{"status": "success"}"""))
+        stubShouldNotBreak(request, response, oldContract, newContract)
+    }
+
+    @Test
+    fun `incompatible change from string to number in string in request body`() {
+        val oldContract = """
+Feature: User API
+  Scenario: Add user
+    Given json User
+    | id | (string) |
+    And json Status
+    | status | (string) |
+    When POST /user
+    And request-body (User)
+    Then status 200
+    And response-body success
+    And response-body (Status)
+        """.trimIndent()
+
+        val newContract = """
+Feature: User API
+  Scenario: Add user
+    Given json User
+    | id | (number in string) |
+    And json Status
+    | status | (string) |
+    When POST /user
+    And request-body (User)
+    Then status 200
+    And response-body success
+    And response-body (Status)
+""".trimIndent()
+
+        newContract notBackwardCompatibleWith oldContract
+
+        val request = HttpRequest("POST","/user", body = parsedJSONStructure("""{"id": "abc"}"""))
+        val response = HttpResponse.OK(parsedJSONStructure("""{"status": "success"}"""))
+        stubShouldBreak(request, response, oldContract, newContract)
+    }
+
+    @Disabled
+    @Test
+    fun `add a key to the response body`() {
+        val newContract = """
+Feature: User API
+  Scenario: Add user
+    Given json User
+    | name | (string) |
+    And json Status
+    | status | (string) |
+    | data   | (string) |
+    When POST /user
+    And request-body (User)
+    Then status 200
+    And response-body (Status)
+""".trimIndent()
+
+        newContract backwardCompatibleWith oldContract
+
+        val request = HttpRequest("POST","/user", body = parsedJSONStructure("""{"name": "John Doe"}"""))
+        val response = HttpResponse.OK(parsedJSONStructure("""{"status": "success"}"""))
+        stubShouldNotBreak(request, response, oldContract, newContract)
+    }
+
+    @Test
+    fun `add an optional key to the response body`() {
+        val newContract = """
+Feature: User API
+  Scenario: Add user
+    Given json User
+    | name | (string) |
+    And json Status
+    | status | (string) |
+    | data?  | (string) |
+    When POST /user
+    And request-body (User)
+    Then status 200
+    And response-body (Status)
+""".trimIndent()
+
+        newContract backwardCompatibleWith oldContract
+
+        val request = HttpRequest("POST","/user", body = parsedJSONStructure("""{"name": "John Doe"}"""))
+        val response = HttpResponse.OK(parsedJSONStructure("""{"status": "success"}"""))
+        stubShouldNotBreak(request, response, oldContract, newContract)
+    }
+}

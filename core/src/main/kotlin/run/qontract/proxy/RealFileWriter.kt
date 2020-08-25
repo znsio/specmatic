b129a3d29d3750beaf8f07967ad4928b31da39f0
Joel Rosario <joel@xnsio.com>
2020-08-25 18:11:13 +0530

    Made the tests pass

---
+++ core/src/main/kotlin/run/qontract/proxy/RealFileWriter.kt
@@ -0,0 +1,16 @@
+package run.qontract.proxy
+
+import java.io.File
+
+class RealFileWriter(private val dataDir: File): FileWriter {
+    constructor(dataDir: String): this(File(dataDir))
+
+    override fun createDirectory() {
+        if (!dataDir.exists()) dataDir.mkdirs()
+    }
+
+    override fun writeText(path: String, content: String) {
+        dataDir.resolve(path).writeText(content)
+    }
+
+}

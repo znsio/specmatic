7bdcf047c91cd3ff398bd725d7399944e47d9818
Joel Rosario <cirquitz@gmail.com>
2020-09-14 17:14:31 +0530

    Removed dead code

--- core/src/main/kotlin/run/qontract/core/TestBackwardCompatibility.kt
+++ core/src/main/kotlin/run/qontract/core/TestBackwardCompatibility.kt
@@ -40,33 +40,3 @@ fun testBackwardCompatibility(older: Feature, newerBehaviour: Feature): Results 
         }
     }
 }
-
-data class Comparison(val older: String, val newer: String, val results: Results)
-
-sealed class CompatibilityResult
-data class TestResults(val list: Sequence<Comparison>) : CompatibilityResult()
-data class JustOne(val filePath: String) : CompatibilityResult()
-object NoContractsFound : CompatibilityResult()
-
-fun testBackwardCompatibilityInDirectory(directory: File, majorVersion: Int, minorVersion: Int?): CompatibilityResult {
-    val files = (directory.listFiles()?.toList() ?: emptyList()).asSequence().filterNotNull().filter {
-        it.name.startsWith("$majorVersion.") && it.name.endsWith(".$QONTRACT_EXTENSION")
-    }.map {
-        val pieces = it.name.removeSuffix(".$QONTRACT_EXTENSION").split(".").filter { it.isNotBlank() }.map { it.toInt() }
-        Triple(it, pieces.first(), pieces.getOrElse(1) { 0 })
-    }.sortedWith(compareBy({it.second}, {it.third}) ).filter { (_, _, fileMinorVersion) ->
-        minorVersion == null || fileMinorVersion <= minorVersion
-    }.map { it.first }
-
-    if(files.elementAtOrNull(0) == null) {
-        return NoContractsFound
-    }
-
-    if(files.elementAtOrNull(1) == null)
-        return JustOne(files.first().path)
-
-    return TestResults(files.zipWithNext().asSequence().map { (older, newer) ->
-        val results = testBackwardCompatibility(Feature(older.readText()), Feature(newer.readText()))
-        Comparison(older.absolutePath, newer.absolutePath, results)
-    })
-}

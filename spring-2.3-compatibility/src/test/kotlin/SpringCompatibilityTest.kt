079421147ab8b295c5445a2397b358b6932ef83a
Joel Rosario <cirquitz@gmail.com>
2021-02-03 17:00:36 +0530

    Added a project specially for replicating spring incompatibility issues. The project exists but we have not yet been able to replicate the issues we are seeing in other projects.

---
+++ spring-2.3-compatibility/src/test/kotlin/SpringCompatibilityTest.kt
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForEntity
+import run.qontract.stub.HttpStub
+import org.assertj.core.api.Assertions.assertThat
+
+class SpringCompatibilityTest {
+    @Test
+    fun twoPointThree() {
+        val simpleContract = """
+Feature: Simple
+  Scenario: Simpler
+    When GET /
+    Then status 200
+    And response-body success
+""".trimIndent()
+
+        val stub = HttpStub(simpleContract)
+
+        val client = RestTemplate()
+        val result = client.getForEntity<String>("http://localhost:9000/")
+        assertThat(result.body).isEqualTo("success")
+    }
+}

1feb5006d8f3edc70b912abee4562785078c55e8
Hari Krishnan <harikrishnan83@gmail.com>
2021-05-06 13:22:37 +0530

    minor cleanup

--- core/src/test/kotlin/in/specmatic/conversions/OpenApiKtTest.kt
+++ core/src/test/kotlin/in/specmatic/conversions/OpenApiKtTest.kt
@@ -11,12 +11,18 @@ import org.springframework.web.client.RestTemplate
 import java.io.File
 import java.net.URI
 import `in`.specmatic.core.*
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.BeforeEach
 import kotlin.test.assertTrue
 
 internal class OpenApiKtTest {
-    @Test
-    fun `should create stub from OpenAPI spec`() {
+    companion object {
+        const val OPENAPI_FILE = "openApiTest.yaml"
+    }
+
+    @BeforeEach
+    fun `setup`() {
         val openAPI = """
 openapi: 3.0.0
 info:
@@ -42,11 +48,19 @@ paths:
                 type: string
     """.trim()
 
-        val openApiFile = File("openApiTest.yaml")
+        val openApiFile = File(OPENAPI_FILE)
         openApiFile.createNewFile()
         openApiFile.writeText(openAPI)
+    }
 
-        val feature = toFeatures(openApiFile.absolutePath)
+    @AfterEach
+    fun `teardown`() {
+        File(OPENAPI_FILE).delete()
+    }
+
+    @Test
+    fun `should create stub from OpenAPI spec`() {
+        val feature = toFeatures(File(OPENAPI_FILE).absolutePath)
 
         val response = HttpStub(feature).use { mock ->
             val restTemplate = RestTemplate()
@@ -54,44 +68,13 @@ paths:
         }
 
         assertThat(response.statusCodeValue).isEqualTo(200)
-
-        openApiFile.delete()
     }
 
     @Test
     fun `should create test from OpenAPI spec`() {
-        val openAPI = """
-openapi: 3.0.0
-info:
-  title: Sample API
-  description: Optional multiline or single-line description in [CommonMark](http://commonmark.org/help/) or HTML.
-  version: 0.1.9
-servers:
-  - url: http://api.example.com/v1
-    description: Optional server description, e.g. Main (production) server
-  - url: http://staging-api.example.com
-    description: Optional server description, e.g. Internal staging server for testing
-paths:
-  /hello:
-    get:
-      summary: hello world
-      description: Optional extended description in CommonMark or HTML.
-      responses:
-        '200':
-          description: Says hello
-          content:
-            application/json:
-              schema:
-                type: string
-    """.trim()
-
-        val openApiFile = File("openApiTest.yaml")
-        openApiFile.createNewFile()
-        openApiFile.writeText(openAPI)
-
         val flags = mutableMapOf<String, Boolean>()
 
-        val feature = toFeatures(openApiFile.absolutePath)
+        val feature = toFeatures(File(OPENAPI_FILE).absolutePath)
 
         val results = feature[0].executeTests(
                 object : TestExecutor {
@@ -113,44 +96,13 @@ paths:
 
         assertThat(flags["executed"]).isTrue
         assertTrue(results.success(), results.report())
-
-        openApiFile.delete()
     }
 
     @Test
     fun `should report error on test from OpenAPI spec with scenario name`() {
-        val openAPI = """
-openapi: 3.0.0
-info:
-  title: Sample API
-  description: Optional multiline or single-line description in [CommonMark](http://commonmark.org/help/) or HTML.
-  version: 0.1.9
-servers:
-  - url: http://api.example.com/v1
-    description: Optional server description, e.g. Main (production) server
-  - url: http://staging-api.example.com
-    description: Optional server description, e.g. Internal staging server for testing
-paths:
-  /hello:
-    get:
-      summary: hello world
-      description: Optional extended description in CommonMark or HTML.
-      responses:
-        '200':
-          description: Says hello
-          content:
-            application/json:
-              schema:
-                type: string
-    """.trim()
-
-        val openApiFile = File("openApiTest.yaml")
-        openApiFile.createNewFile()
-        openApiFile.writeText(openAPI)
-
         val flags = mutableMapOf<String, Boolean>()
 
-        val feature = toFeatures(openApiFile.absolutePath)
+        val feature = toFeatures(File(OPENAPI_FILE).absolutePath)
 
         val results = feature[0].executeTests(
                 object : TestExecutor {
@@ -178,7 +130,5 @@ paths:
 
             Expected status: 200, actual: 400
         """.trimIndent())
-
-        openApiFile.delete()
     }
 }

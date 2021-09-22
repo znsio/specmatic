361e2aca8de6f84373d725d4b95ca9601b42023e
Hari Krishnan <harikrishnan83@gmail.com>
2021-09-22 13:42:54 +0530

    adding external command tests

---
+++ core/src/test/kotlin/in/specmatic/core/utilities/ExternalCommandTest.kt
@@ -0,0 +1,39 @@
+package `in`.specmatic.core.utilities
+
+import `in`.specmatic.core.pattern.ContractException
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.DisabledOnOs
+import org.junit.jupiter.api.condition.OS
+
+internal class ExternalCommandTest {
+    @Test
+    fun `should throw contract exception when external command does not exist`() {
+        val exception = assertThrows(ContractException::class.java) {
+            ExternalCommand(arrayOf("missing_command"), ".", emptyArray()).executeAsSeparateProcess()
+        }
+        assertThat(exception.report()).isEqualTo(
+            """Error running missing_command: Cannot run program "missing_command" (in directory "."): error=2, No such file or directory"""
+        )
+    }
+
+    @Test
+    @DisabledOnOs(OS.WINDOWS)
+    fun `should throw contract exception when external command returns non zero exit code`() {
+        val exception = assertThrows(ContractException::class.java) {
+            ExternalCommand(arrayOf("cat", "missing_file"), ".", emptyArray()).executeAsSeparateProcess()
+        }
+        assertThat(exception.report()).isEqualTo(
+            """Error executing cat missing_file: cat: missing_file: No such file or directory"""
+        )
+    }
+
+    @Test
+    @DisabledOnOs(OS.WINDOWS)
+    fun `should return command output`() {
+        val commandOutput =
+            ExternalCommand(arrayOf("echo", "hello"), ".", emptyArray()).executeAsSeparateProcess()
+        assertThat(commandOutput).isEqualTo("hello\n")
+    }
+}

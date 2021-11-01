ae9a04dcf00a6168c9c4f44cb1a7a029bac851eb
Joel Rosario <cirquitz@gmail.com>
2021-11-01 12:08:15 +0530

    Revert "trying to fix failing tests"
    This reverts commit 61bd685aa7c20b27b315e8d29f3c460066a66dbd.

--- core/src/test/kotlin/in/specmatic/core/log/CompositePrinterTest.kt
+++ core/src/test/kotlin/in/specmatic/core/log/CompositePrinterTest.kt
@@ -1,25 +1,25 @@
 package `in`.specmatic.core.log
-//
-//import org.assertj.core.api.Assertions.assertThat
-//import org.junit.jupiter.api.Test
-//
-//internal class CompositePrinterTest {
-//    @Test
-//    fun `should write to all available printers`() {
-//        var printed = 0
-//
-//        val printer = CompositePrinter().apply {
-//            printers.clear()
-//            printers.add(object: LogPrinter {
-//                override fun print(msg: LogMessage) {
-//                    assertThat(msg.toLogString()).isEqualTo("Hello")
-//                    printed += 1
-//                }
-//            })
-//        }
-//
-//        printer.print(StringLog("Hello"))
-//        assertThat(printed).isOne
-//    }
-//
-//}
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class CompositePrinterTest {
+    @Test
+    fun `should write to all available printers`() {
+        var printed = 0
+
+        val printer = CompositePrinter().apply {
+            printers.clear()
+            printers.add(object: LogPrinter {
+                override fun print(msg: LogMessage) {
+                    assertThat(msg.toLogString()).isEqualTo("Hello")
+                    printed += 1
+                }
+            })
+        }
+
+        printer.print(StringLog("Hello"))
+        assertThat(printed).isOne
+    }
+
+}

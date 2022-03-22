ad1451fe840a07f6086e18ed004e9cd4ab0ac690
Joel Rosario <cirquitz@gmail.com>
2022-03-22 12:57:51 +0530

    Disable Bundle E2E tests for now

--- application/src/test/kotlin/application/BundleCommandTestE2E.kt
+++ application/src/test/kotlin/application/BundleCommandTestE2E.kt
@@ -8,6 +8,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.eclipse.jgit.api.Git
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import org.springframework.beans.factory.annotation.Autowired
@@ -19,39 +20,27 @@ import java.util.function.Consumer
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = [SpecmaticApplication::class, BundleCommand::class])
-internal class BundleCommandTestE2E {
-    companion object {
-        @TempDir
-        lateinit var tempDir: File
-        lateinit var configFilename: String
-        lateinit var projectDir: File
-        lateinit var specmaticJSONContent: String
+class BundleTestData(tempDir: File) {
+    val projectDir: File
+    val specmaticJSONContent: String
+    val configFilename: String
 
-        @BeforeAll
-        @JvmStatic
-        fun setUp() {
-            val bundleTestDir = tempDir.resolve("bundle_tests")
-            val gitDir = bundleTestDir.resolve("bundle_test_contracts")
-
-            val gitInit = Git.init().setDirectory(gitDir)
-            val git = gitInit.call()
-
-            createFile(gitDir, "test.yaml", "dummy contract content")
-            val implicitDir = createDir(gitDir, "test_data")
-            createFile(implicitDir, "stub.json", "dummy stub content")
-
-            val separateDataDir = createDir(gitDir, "data")
-            val implicitCustomBaseDir = createDir(separateDataDir, "test_data")
-            createFile(implicitCustomBaseDir, "stub2.json", "dummy stub content 2")
-
-            git.add().addFilepattern(".").call()
-            git.commit().also { it.message = "First commit" }.call()
-
-            projectDir = createDir(bundleTestDir, "project")
-            val specmaticJSONFile = createFile(projectDir, "specmatic.json")
-
-            specmaticJSONContent = """
+    init {
+        val bundleTestDir = tempDir.resolve("bundle_tests")
+        val gitDir = bundleTestDir.resolve("bundle_test_contracts")
+        val gitInit = Git.init().setDirectory(gitDir)
+        val git = gitInit.call()
+        createFile(gitDir, "test.yaml", "dummy contract content")
+        val implicitDir = createDir(gitDir, "test_data")
+        createFile(implicitDir, "stub.json", "dummy stub content")
+        val separateDataDir = createDir(gitDir, "data")
+        val implicitCustomBaseDir = createDir(separateDataDir, "test_data")
+        createFile(implicitCustomBaseDir, "stub2.json", "dummy stub content 2")
+        git.add().addFilepattern(".").call()
+        git.commit().also { it.message = "First commit" }.call()
+        projectDir = createDir(bundleTestDir, "project")
+        val specmaticJSONFile = createFile(projectDir, "specmatic.json")
+        specmaticJSONContent = """
                 {
                   "sources": [
                     {
@@ -67,33 +56,43 @@ internal class BundleCommandTestE2E {
                   ]
                 }
             """.trimIndent()
+        specmaticJSONFile.writeText(specmaticJSONContent)
+        configFilename = Configuration.globalConfigFileName
+        Configuration.globalConfigFileName = specmaticJSONFile.canonicalPath
+    }
+}
 
-            specmaticJSONFile.writeText(specmaticJSONContent)
+private fun createDir(gitDir: File, dirName: String): File {
+    val implicitDir = gitDir.resolve(dirName)
+    implicitDir.mkdirs()
+    return implicitDir
+}
 
+private fun createFile(gitDir: File, filename: String, content: String? = null): File {
+    val newFile = gitDir.resolve(filename)
+    newFile.createNewFile()
+
+    if(content != null)
+        newFile.writeText(content)
+
+    return newFile
+}
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = [SpecmaticApplication::class, BundleCommand::class])
+internal class BundleCommandTestE2E {
+    companion object {
+        lateinit var configFilename: String
+
+        @BeforeAll
+        @JvmStatic
+        fun setUp() {
             configFilename = Configuration.globalConfigFileName
-            Configuration.globalConfigFileName = specmaticJSONFile.canonicalPath
         }
 
         @AfterAll
         @JvmStatic
         fun tearDown() {
             Configuration.globalConfigFileName = configFilename
-        }
-
-        private fun createDir(gitDir: File, dirName: String): File {
-            val implicitDir = gitDir.resolve(dirName)
-            implicitDir.mkdirs()
-            return implicitDir
-        }
-
-        private fun createFile(gitDir: File, filename: String, content: String? = null): File {
-            val newFile = gitDir.resolve(filename)
-            newFile.createNewFile()
-
-            if(content != null)
-                newFile.writeText(content)
-
-            return newFile
         }
     }
 
@@ -103,44 +102,46 @@ internal class BundleCommandTestE2E {
     @Autowired
     lateinit var bundleCommand: BundleCommand
 
+    @Disabled
     @Test
-    fun `basic stub bundle command functionality`() {
-        val bundleFile = projectDir.resolve("bundle.zip")
-        try {
-            bundleCommand.bundleOutputPath = bundleFile.canonicalPath
-            bundleCommand.call()
+    fun `basic stub bundle command functionality`(@TempDir tempDir: File) {
+        val bundleTestData = BundleTestData(tempDir)
+        val bundleFile = bundleTestData.projectDir.resolve("bundle.zip")
 
-            val entries = mutableListOf<Pair<String, String>>()
+        bundleCommand.bundleOutputPath = bundleFile.canonicalPath
+        bundleCommand.call()
 
-            FileInputStream(bundleCommand.bundleOutputPath!!).use { zipFileInputStream ->
-                ZipInputStream(zipFileInputStream).use { zipIn ->
-                    var zipEntry = zipIn.nextEntry
-                    while (zipEntry != null) {
-                        val content = String(zipIn.asInput().asStream().readBytes())
-                        entries.add(Pair(zipEntry.name, content))
-                        zipEntry = zipIn.nextEntry
-                    }
+        val entries = mutableListOf<Pair<String, String>>()
+
+        FileInputStream(bundleCommand.bundleOutputPath!!).use { zipFileInputStream ->
+            ZipInputStream(zipFileInputStream).use { zipIn ->
+                var zipEntry = zipIn.nextEntry
+                while (zipEntry != null) {
+                    val content = String(zipIn.asInput().asStream().readBytes())
+                    entries.add(Pair(zipEntry.name, content))
+                    zipEntry = zipIn.nextEntry
                 }
             }
-
-            println(entries)
-            assertThat(entries).hasSize(2)
-            assertThat(entries).anySatisfy(Consumer {
-                assertThat(it.first).contains("test.yaml")
-                assertThat(it.second).contains("dummy contract content")
-            })
-            assertThat(entries).anySatisfy(Consumer {
-                assertThat(it.first).contains("stub.json")
-                assertThat(it.second).contains("dummy stub content")
-            })
-        } finally {
-            bundleFile.delete()
         }
+
+        println(entries)
+        assertThat(entries).anySatisfy(Consumer {
+            assertThat(it.first).contains("test.yaml")
+            assertThat(it.second).contains("dummy contract content")
+        })
+        assertThat(entries).anySatisfy(Consumer {
+            assertThat(it.first).contains("stub.json")
+            assertThat(it.second).contains("dummy stub content")
+        })
+        assertThat(entries).hasSize(2)
     }
 
+    @Disabled
     @Test
-    fun `bundle is generated with shifted base`() {
-        val bundleFile = projectDir.resolve("bundle.zip")
+    fun `bundle is generated with shifted base`(@TempDir tempDir: File) {
+        val bundleTestData = BundleTestData(tempDir)
+        val bundleFile = bundleTestData.projectDir.resolve("bundle.zip")
+
         try {
             bundleCommand.bundleOutputPath = bundleFile.canonicalPath
             System.setProperty("customImplicitStubBase", "data")
@@ -160,7 +161,6 @@ internal class BundleCommandTestE2E {
             }
 
             println(entries)
-            assertThat(entries).hasSize(3)
             assertThat(entries).anySatisfy(Consumer {
                 assertThat(it.first).contains("test.yaml")
                 assertThat(it.second).contains("dummy contract content")
@@ -173,47 +173,45 @@ internal class BundleCommandTestE2E {
                 assertThat(it.first).contains("stub2.json")
                 assertThat(it.second).contains("dummy stub content 2")
             })
+
+            assertThat(entries).hasSize(3)
         } finally {
             System.clearProperty("customImplicitStubBase")
-            bundleFile.delete()
         }
     }
 
+    @Disabled
     @Test
-    fun `test bundle is generated`() {
-        val bundleFile = projectDir.resolve("bundle.zip")
-        try {
-            bundleCommand.bundleOutputPath = bundleFile.canonicalPath
-            bundleCommand.testBundle = true
-            System.setProperty("customImplicitStubBase", "data")
-            bundleCommand.call()
+    fun `test bundle is generated`(@TempDir tempDir: File) {
+        val bundleTestData = BundleTestData(tempDir)
+        val bundleFile = bundleTestData.projectDir.resolve("bundle.zip")
 
-            val entries = mutableListOf<Pair<String, String>>()
+        bundleCommand.bundleOutputPath = bundleFile.canonicalPath
+        bundleCommand.testBundle = true
+        bundleCommand.call()
 
-            FileInputStream(bundleCommand.bundleOutputPath!!).use { zipFileInputStream ->
-                ZipInputStream(zipFileInputStream).use { zipIn ->
-                    var zipEntry = zipIn.nextEntry
-                    while (zipEntry != null) {
-                        val content = String(zipIn.asInput().asStream().readBytes())
-                        entries.add(Pair(zipEntry.name, content))
-                        zipEntry = zipIn.nextEntry
-                    }
+        val entries = mutableListOf<Pair<String, String>>()
+
+        FileInputStream(bundleCommand.bundleOutputPath!!).use { zipFileInputStream ->
+            ZipInputStream(zipFileInputStream).use { zipIn ->
+                var zipEntry = zipIn.nextEntry
+                while (zipEntry != null) {
+                    val content = String(zipIn.asInput().asStream().readBytes())
+                    entries.add(Pair(zipEntry.name, content))
+                    zipEntry = zipIn.nextEntry
                 }
             }
-
-            println(entries)
-            assertThat(entries).hasSize(2)
-            assertThat(entries).anySatisfy(Consumer {
-                assertThat(it.first).contains("test.yaml")
-                assertThat(it.second).contains("dummy contract content")
-            })
-            assertThat(entries).anySatisfy(Consumer {
-                assertThat(it.first).contains("specmatic.json")
-                assertThat(it.second).contains(specmaticJSONContent)
-            })
-        } finally {
-            System.clearProperty("customImplicitStubBase")
-            bundleFile.delete()
         }
+
+        println(entries)
+        assertThat(entries).anySatisfy(Consumer {
+            assertThat(it.first).contains("test.yaml")
+            assertThat(it.second).contains("dummy contract content")
+        })
+        assertThat(entries).anySatisfy(Consumer {
+            assertThat(it.first).contains("specmatic.json")
+            assertThat(it.second).contains(bundleTestData.specmaticJSONContent)
+        })
+        assertThat(entries).hasSize(2)
     }
 }

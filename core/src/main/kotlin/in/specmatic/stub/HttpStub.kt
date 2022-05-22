ac87ba6095a829f3dc7e4bae832f71445eb0062b
Hari Krishnan <harikrishnan83@gmail.com>
2022-05-22 12:42:07 +0530

    adding sse

--- core/src/main/kotlin/in/specmatic/stub/HttpStub.kt
+++ core/src/main/kotlin/in/specmatic/stub/HttpStub.kt
@@ -30,6 +30,9 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.util.asStream
 import io.ktor.util.toMap
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.broadcast
+import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import java.io.ByteArrayOutputStream
@@ -83,8 +86,25 @@ class HttpStub(private val features: List<Feature>, _httpStubs: List<HttpStubDat
                         else -> serveStubResponse(httpRequest)
                     }
 
-                    respondToKtorHttpResponse(call, httpStubResponse.response, httpStubResponse.delayInSeconds)
-                    httpLogMessage.addResponse(httpStubResponse)
+                    if(httpRequest.headers["Content-Type"] == "text/event-stream") {
+                        val channel = produce { // this: ProducerScope<SseEvent> ->
+                            var n = 0
+                            while (true) {
+                                send(SseEvent("demo$n"))
+                                delay(1000)
+                                n++
+                            }
+                        }.broadcast()
+                        val events = channel.openSubscription()
+                        try {
+                            call.respondSse(events)
+                        } finally {
+                            events.cancel()
+                        }
+                    } else {
+                        respondToKtorHttpResponse(call, httpStubResponse.response, httpStubResponse.delayInSeconds)
+                        httpLogMessage.addResponse(httpStubResponse)
+                    }
                 }
                 catch(e: ContractException) {
                     val response = badRequest(e.report())
@@ -481,4 +501,25 @@ fun stringToMockScenario(text: Value): ScenarioStub {
             }
 
     return mockFromJSON(mockSpec)
+}
+
+data class SseEvent(val data: String, val event: String? = null, val id: String? = null)
+
+suspend fun ApplicationCall.respondSse(events: ReceiveChannel<SseEvent>) {
+    response.cacheControl(CacheControl.NoCache(null))
+    respondTextWriter(contentType = ContentType.Text.EventStream) {
+        for (event in events) {
+            if (event.id != null) {
+                write("id: ${event.id}\n")
+            }
+            if (event.event != null) {
+                write("event: ${event.event}\n")
+            }
+            for (dataLine in event.data.lines()) {
+                write("data: $dataLine\n")
+            }
+            write("\n")
+            flush()
+        }
+    }
 }

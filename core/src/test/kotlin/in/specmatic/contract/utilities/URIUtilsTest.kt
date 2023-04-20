71810cc4efa0e81ff85bc3679db24a55d307b441
nikhil081 <nikmakkad08@gmail.com>
2023-04-21 00:43:57 +0530

    added test case for UriUtilsTest.kt and upgraded gradle wrapper version

--- core/src/test/kotlin/in/specmatic/contract/utilities/URIUtilsTest.kt
+++ core/src/test/kotlin/in/specmatic/contract/utilities/URIUtilsTest.kt
@@ -1,8 +1,10 @@
 package `in`.specmatic.contract.utilities
 
 import `in`.specmatic.core.utilities.URIUtils.parsePathParams
+import `in`.specmatic.core.utilities.URIUtils.parseQuery
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.net.URI
 import java.net.URISyntaxException
 
@@ -22,5 +24,30 @@ internal class URIUtilsTest {
         val pathParameters = parsePathParams(uri.rawPath)
         Assertions.assertEquals(pathParameters["petid"], "(number)")
         Assertions.assertEquals(pathParameters["owner"], "(string)")
+    }
+
+    @Test
+
+    fun given_ParseQuery_When_Null_Then_ThrowsException() {
+        val resultMap = parseQuery(null)
+        Assertions.assertEquals(resultMap, emptyMap<String, String>())
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun given_ParseQuery_When_InValidString_Then_ThrowsException() {
+        assertThrows<Exception> {
+            parseQuery("abc&xyz")
+        }.also {
+            org.assertj.core.api.Assertions.assertThat(it.message)
+                .isEqualTo("a part of the query string does not seem to be a key-value pair: abc")
+        }
+    }
+
+    @Test
+    fun given_ParseQuery_When_ValidString_Then_ReturnEmptyStringWithValidKeys() {
+        val resultantMap = parseQuery("abc=&xyz=")
+        Assertions.assertEquals(resultantMap["abc"], "")
+        Assertions.assertEquals(resultantMap["xyz"], "")
     }
 }

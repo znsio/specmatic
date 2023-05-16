4ca98fae5397b17a215737803ddf3d51e81a4524
André Roaldseth <andre@roaldseth.net>
2023-05-16 13:43:54 +0200

    Add support for generating valid dates

--- core/src/main/kotlin/in/specmatic/core/pattern/DatePattern.kt
+++ core/src/main/kotlin/in/specmatic/core/pattern/DatePattern.kt
@@ -8,29 +8,29 @@ import `in`.specmatic.core.value.Value
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-object DateTimePattern : Pattern, ScalarType {
+object DatePattern : Pattern, ScalarType {
     override fun matches(sampleData: Value?, resolver: Resolver): Result = when (sampleData) {
         is StringValue -> resultOf {
             parse(sampleData.string, resolver)
             Result.Success()
         }
-        else -> Result.Failure("DateTime types can only be represented using strings")
+        else -> Result.Failure("Date types can only be represented using strings")
     }
 
     override fun generate(resolver: Resolver): StringValue = currentDateTime()
 
-    override fun newBasedOn(row: Row, resolver: Resolver): List<DateTimePattern> = listOf(this)
+    override fun newBasedOn(row: Row, resolver: Resolver): List<DatePattern> = listOf(this)
 
-    override fun newBasedOn(resolver: Resolver): List<DateTimePattern> = listOf(this)
+    override fun newBasedOn(resolver: Resolver): List<DatePattern> = listOf(this)
     override fun negativeBasedOn(row: Row, resolver: Resolver): List<Pattern> {
         return listOf(NullPattern)
     }
 
     override fun parse(value: String, resolver: Resolver): StringValue =
-            attempt {
-                DateTimeFormatter.ISO_DATE_TIME.parse(value)
-                StringValue(value)
-            }
+        attempt {
+            DateTimeFormatter.ISO_DATE.parse(value)
+            StringValue(value)
+        }
 
     override fun encompasses(otherPattern: Pattern, thisResolver: Resolver, otherResolver: Resolver, typeStack: TypeStack): Result {
         return encompasses(this, otherPattern, thisResolver, otherResolver, typeStack)
@@ -43,11 +43,11 @@ object DateTimePattern : Pattern, ScalarType {
     override val typeAlias: String?
         get() = null
 
-    override val typeName: String = "datetime"
+    override val typeName: String = "date"
 
-    override val pattern = "(datetime)"
+    override val pattern = "(date)"
 
     override fun toString() = pattern
 }
 
-private fun currentDateTime() = StringValue(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME))
+private fun currentDateTime() = StringValue(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))

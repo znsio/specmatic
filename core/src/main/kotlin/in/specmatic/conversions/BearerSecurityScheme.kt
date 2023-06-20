6d07a08bb3170182c9a0b7495407f75d157ec347
André Roaldseth <andre@roaldseth.net>
2023-06-20 10:19:57 +0200

    Change return type of OpenAPISecurityScheme::matches() from boolean to Result
    This allows more verbose feedback on why the authentication mechanism did not match the request

--- core/src/main/kotlin/in/specmatic/conversions/BearerSecurityScheme.kt
+++ core/src/main/kotlin/in/specmatic/conversions/BearerSecurityScheme.kt
@@ -3,15 +3,20 @@ package `in`.specmatic.conversions
 import `in`.specmatic.core.HttpRequest
 import `in`.specmatic.core.HttpRequestPattern
 import `in`.specmatic.core.Resolver
+import `in`.specmatic.core.Result
 import `in`.specmatic.core.pattern.Row
 import `in`.specmatic.core.pattern.StringPattern
 import org.apache.http.HttpHeaders.AUTHORIZATION
 
 class BearerSecurityScheme : OpenAPISecurityScheme {
-    override fun matches(httpRequest: HttpRequest): Boolean {
-        return httpRequest.headers.let {
-            it.containsKey(AUTHORIZATION) && it[AUTHORIZATION]!!.lowercase().startsWith("bearer")
+    override fun matches(httpRequest: HttpRequest): Result {
+        httpRequest.headers.let {
+            if (!it.containsKey(AUTHORIZATION))
+                return Result.Failure("$AUTHORIZATION header is missing in request")
+            if (!it[AUTHORIZATION]!!.lowercase().startsWith("bearer"))
+                return Result.Failure("$AUTHORIZATION header must be prefixed with \"Bearer\"")
         }
+        return Result.Success()
     }
 
     override fun removeParam(httpRequest: HttpRequest): HttpRequest {
--- core/src/test/kotlin/in/specmatic/conversions/BearerSecuritySchemeTest.kt
+++ core/src/test/kotlin/in/specmatic/conversions/BearerSecuritySchemeTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 
 class BearerSecuritySchemeTest {
     @Test
-    fun `authentication header starts with Bearer when using Bearer secuirty scheme`() {
+    fun `authentication header starts with Bearer when using Bearer security scheme`() {
         val scheme = BearerSecurityScheme()
         val request = scheme.addTo(
             HttpRequest(
@@ -16,5 +16,25 @@ class BearerSecuritySchemeTest {
             ),
         )
         assertThat(request.headers[AUTHORIZATION]).startsWith("Bearer ")
+    }
+
+    @Test
+    fun `Bearer security scheme matches requests with authorization header set`() {
+        val scheme = BearerSecurityScheme()
+
+        val requestWithBearer = HttpRequest(method = "POST", path = "/customer", headers = mapOf(AUTHORIZATION to "Bearer foo"))
+        assertThat(scheme.matches(requestWithBearer).isSuccess()).isTrue
+
+        val requestWithoutHeader = HttpRequest(method = "POST", path = "/customer")
+        with(scheme.matches(requestWithoutHeader)) {
+            assertThat(isSuccess()).isFalse
+            assertThat(this.reportString()).contains("Authorization header is missing in request")
+        }
+
+        val requestWithoutBearer = HttpRequest(method = "POST", path = "/customer", headers = mapOf(AUTHORIZATION to "foo"))
+        with(scheme.matches(requestWithoutBearer)) {
+            assertThat(isSuccess()).isFalse
+            assertThat(this.reportString()).contains("must be prefixed")
+        }
     }
 }

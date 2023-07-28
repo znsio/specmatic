7b3aa33c0daf04a4b2b8b4544c1afa3c6ed51e43
Vikram Rao <contact@vikramrao.in>
2023-07-28 17:09:03 +0530

    More tests for api coverage fix (multiple path parameters)

--- core/src/test/kotlin/in/specmatic/conversions/SpringRequestMappingKtTest.kt
+++ core/src/test/kotlin/in/specmatic/conversions/SpringRequestMappingKtTest.kt
@@ -5,9 +5,21 @@ import org.junit.jupiter.api.Test
 
 class SpringRequestMappingKtTest {
     @Test
-    fun `convert common brackets to braces when designating path parameters in url`(): Unit {
-        val path = "/(country:string)/state/(state:number)";
+    fun `convert common brackets to braces when designating sigle path parameters in url`(): Unit {
+        val path = "/(country:string)/state";
+        assertEquals("/{country}/state", convertPathParameterStyle(path))
+    }
+
+    @Test
+    fun `convert common brackets to braces when designating double path parameters in url`(): Unit {
+        val path = "/(country:string)/state/(state:string)";
         assertEquals("/{country}/state/{state}", convertPathParameterStyle(path))
+    }
+
+    @Test
+    fun `convert common brackets to braces when designating multiple path parameters in url`(): Unit {
+        val path = "/(country:string)/state/(state:string)/(pincode:number)";
+        assertEquals("/{country}/state/{state}/{pincode}", convertPathParameterStyle(path))
     }
 }
 

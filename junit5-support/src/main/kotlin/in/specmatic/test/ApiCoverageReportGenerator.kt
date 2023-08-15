e35753bfe96b39cded91313001e0161dc3e963f0
jaydeep <jaydeepk@gmail.com>
2023-08-15 16:30:56 +0530

    Refactored the generation of APICoverage rows to potentially cater to generating coverage %age

--- junit5-support/src/main/kotlin/in/specmatic/test/ApiCoverageReportGenerator.kt
+++ junit5-support/src/main/kotlin/in/specmatic/test/ApiCoverageReportGenerator.kt
@@ -14,38 +14,45 @@ class ApiCoverageReportGenerator(
             it.copy(path = convertPathParameterStyle(it.path))
         }
 
-        val coveredAPIRows = recordsWithFixedURLs.groupBy {
+        val coveredAPIs = recordsWithFixedURLs.groupBy {
             "${it.path}-${it.method}-${it.responseStatus}"
         }.let { sortedRecords: Map<String, List<TestResultRecord>> ->
             sortedRecords.keys.sorted().map { key ->
                 sortedRecords.getValue(key)
             }
-        }.let { groupedRecords: List<List<TestResultRecord>> ->
-            groupedRecords.fold(emptyList()) { acc: List<APICoverageRow>, record: List<TestResultRecord> ->
-                val stat = record.first().let {
-                    APICoverageRow(
-                        it.method,
-                        it.path,
-                        it.responseStatus,
-                        record.size,
-                        CoverageStatus.Covered
-                    )
-                }
+        }.flatten()
 
-                when (acc) {
-                    emptyList<APICoverageRow>() -> listOf(stat)
-                    else -> {
-                        val checkedPath =
-                            if (stat.path == acc.lastOrNull { it.path.isNotEmpty() }?.path) stat.copy(path = "") else stat
-                        val checkedMethod =
-                            if (checkedPath.method == acc.lastOrNull { it.method.isNotEmpty() }?.method) checkedPath.copy(
-                                method = ""
-                            ) else checkedPath
+        val groupedAPIs = mutableMapOf<String, MutableMap<String, MutableMap<Int, MutableList<TestResultRecord>>>>()
+        for (result in coveredAPIs) {
+            groupedAPIs.getOrPut(result.path) { mutableMapOf() }
+                .getOrPut(result.method) { mutableMapOf() }
+                .getOrPut(result.responseStatus) { mutableListOf() }
+                .add(result)
+        }
+        val coveredAPIRows:MutableList<APICoverageRow> = mutableListOf()
 
-                        acc.plus(checkedMethod)
+        groupedAPIs.forEach { (route, methodMap) ->
+            val routeAPIRows: MutableList<APICoverageRow> = mutableListOf()
+            val topLevelCoverageRow = createTopLevelApiCoverageRow(route, methodMap)
+            methodMap.forEach { (method, responseCodeMap) ->
+                responseCodeMap.forEach { (responseStatus, testResults) ->
+                    if (routeAPIRows.isEmpty()) {
+                        routeAPIRows.add(topLevelCoverageRow)
+                    } else {
+                        val lastCoverageRow = routeAPIRows.last()
+                        val rowMethod = if (method != lastCoverageRow.method) method else ""
+                        routeAPIRows.add(
+                            topLevelCoverageRow.copy(
+                                method = rowMethod,
+                                path ="",
+                                responseStatus = responseStatus.toString(),
+                                count = testResults.count().toString(),
+                            )
+                        )
                     }
                 }
             }
+            coveredAPIRows.addAll(routeAPIRows)
         }
 
         val testedAPIs = testReportRecords.map { "${it.method}-${it.path}" }
@@ -59,5 +66,21 @@ class ApiCoverageReportGenerator(
         }
 
         return APICoverageReport(coveredAPIRows, missedAPIRows)
+    }
+
+    private fun createTopLevelApiCoverageRow(
+        route: String,
+        methodMap: MutableMap<String, MutableMap<Int, MutableList<TestResultRecord>>>,
+    ): APICoverageRow {
+        val method = methodMap.keys.first()
+        val responseCode = methodMap[method]?.keys?.first()
+        val count =  methodMap[method]?.get(responseCode)?.size
+        return APICoverageRow(
+            method,
+            route,
+            responseCode!!,
+            count!!,
+            CoverageStatus.Covered
+        )
     }
 }

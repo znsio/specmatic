3df313149578c518ee84ebe875cbd55594a07209
jaydeep <jaydeepk@gmail.com>
2023-08-21 22:29:17 +0530

    Explicitly logged error messages when failure criteria is met

--- junit5-support/src/main/kotlin/in/specmatic/test/reports/OpenApiCoverageReportProcessor.kt
+++ junit5-support/src/main/kotlin/in/specmatic/test/reports/OpenApiCoverageReportProcessor.kt
@@ -12,20 +12,17 @@ import org.assertj.core.api.Assertions.assertThat
 class OpenApiCoverageReportProcessor(private val reportConfiguration: ReportConfiguration) {
 
     fun process(openApiCoverageReportInput: OpenApiCoverageReportInput) {
-        val reportTypes = reportConfiguration.types
-        reportTypes.apiCoverage.openAPI.let { openApi ->
-            openApiCoverageReportInput.addExcludedAPIs(openApi.excludedEndpoints)
-            val openAPICoverageReport = openApiCoverageReportInput.generate()
-            if (openAPICoverageReport.rows.isEmpty()) {
-                logger.log("The Open API coverage report generated is blank")
-            } else {
-                val renderers = configureOpenApiCoverageReportRenderers()
-                renderers.forEach { renderer ->
-                    logger.log(renderer.render(openAPICoverageReport))
-                }
+        openApiCoverageReportInput.addExcludedAPIs(reportConfiguration.types.apiCoverage.openAPI.excludedEndpoints)
+        val openAPICoverageReport = openApiCoverageReportInput.generate()
+        if (openAPICoverageReport.rows.isEmpty()) {
+            logger.log("The Open API coverage report generated is blank")
+        } else {
+            val renderers = configureOpenApiCoverageReportRenderers()
+            renderers.forEach { renderer ->
+                logger.log(renderer.render(openAPICoverageReport))
             }
-            assertFailureCriteria(openAPICoverageReport)
         }
+        assertFailureCriteria(openAPICoverageReport)
     }
 
     private fun configureOpenApiCoverageReportRenderers(): List<ReportRenderer<OpenAPICoverageReport>> {
@@ -40,9 +37,19 @@ class OpenApiCoverageReportProcessor(private val reportConfiguration: ReportConf
     private fun assertFailureCriteria(openAPICoverageReport: OpenAPICoverageReport) {
         val failureCriteria = reportConfiguration.types.apiCoverage.openAPI.failureCriteria
         if (failureCriteria.enforce) {
-            assertThat(openAPICoverageReport.totalCoveragePercentage).withFailMessage("Total coverage: ${openAPICoverageReport.totalCoveragePercentage}% is less than the specified minimum threshold of ${failureCriteria.minThresholdPercentage}%")
+            val coverageThresholdNotMetMessage =
+                "Total coverage: ${openAPICoverageReport.totalCoveragePercentage}% is less than the specified minimum threshold of ${failureCriteria.minThresholdPercentage}%"
+            val missedEndpointsCountExceededMessage =
+                "Total missed endpoints count: ${openAPICoverageReport.missedEndpointsCount}% is greater than the maximum threshold of ${failureCriteria.maxMissedEndpointsInSpec}"
+            if(openAPICoverageReport.totalCoveragePercentage < failureCriteria.minThresholdPercentage) {
+                logger.log(coverageThresholdNotMetMessage)
+            }
+            if(openAPICoverageReport.missedEndpointsCount > failureCriteria.maxMissedEndpointsInSpec) {
+                logger.log(missedEndpointsCountExceededMessage)
+            }
+            assertThat(openAPICoverageReport.totalCoveragePercentage).withFailMessage(coverageThresholdNotMetMessage)
                 .isGreaterThanOrEqualTo(failureCriteria.minThresholdPercentage)
-            assertThat(openAPICoverageReport.missedEndpointsCount).withFailMessage("Total missed endpoints count: ${openAPICoverageReport.missedEndpointsCount}% is greater than the maximum threshold of ${failureCriteria.maxMissedEndpointsInSpec}")
+            assertThat(openAPICoverageReport.missedEndpointsCount).withFailMessage(missedEndpointsCountExceededMessage)
                 .isLessThanOrEqualTo(failureCriteria.maxMissedEndpointsInSpec)
         }
     }

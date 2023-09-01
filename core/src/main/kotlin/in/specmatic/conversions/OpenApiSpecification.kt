e1b1b34811d8033410460c5940c727fe48430ae6
Snorkell <samgaming29@live.fr>
2023-09-01 14:37:25 +0200

    Handle byte array string as binarySchema

--- core/src/main/kotlin/in/specmatic/conversions/OpenApiSpecification.kt
+++ core/src/main/kotlin/in/specmatic/conversions/OpenApiSpecification.kt
@@ -627,6 +627,7 @@ class OpenApiSpecification(private val openApiFile: String, val openApi: OpenAPI
                     throw UnsupportedOperationException("Unsupported composed schema: $schema")
                 }
             }
+            is ByteArraySchema -> BinaryPattern()
             else -> {
                 if (schema.nullable == true && schema.additionalProperties == null && schema.`$ref` == null) {
                     NullPattern

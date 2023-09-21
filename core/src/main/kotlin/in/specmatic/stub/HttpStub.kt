a346756b10400e899e5b58720ad1399457c1cc29
Joel Rosario <cirquitz@gmail.com>
2023-09-21 10:59:25 +0530

    Extracted stubbed response classes to separate files

--- core/src/main/kotlin/in/specmatic/stub/HttpStub.kt
+++ core/src/main/kotlin/in/specmatic/stub/HttpStub.kt
@@ -615,35 +615,6 @@ internal suspend fun respondToKtorHttpResponse(call: ApplicationCall, httpRespon
     call.respond(textContent)
 }
 
-interface StubbedResponseResult {
-    fun log(logs: MutableList<StubEndpoint>, httpRequest: HttpRequest)
-
-    val response: HttpStubResponse
-}
-
-class FoundStubbedResponse(override val response: HttpStubResponse) : StubbedResponseResult {
-    override fun log(logs: MutableList<StubEndpoint>, httpRequest: HttpRequest) {
-        logs.add(
-            StubEndpoint(
-                response.scenario?.path,
-                httpRequest.method,
-                response.response.status,
-                response.feature?.sourceProvider,
-                response.feature?.sourceRepository,
-                response.feature?.sourceRepositoryBranch,
-                response.feature?.specification,
-                response.feature?.serviceType,
-            )
-        )
-    }
-}
-
-class NotStubbed(override val response: HttpStubResponse) : StubbedResponseResult {
-    override fun log(logs: MutableList<StubEndpoint>, httpRequest: HttpRequest) {
-
-    }
-}
-
 fun getHttpResponse(
     httpRequest: HttpRequest,
     features: List<Feature>,

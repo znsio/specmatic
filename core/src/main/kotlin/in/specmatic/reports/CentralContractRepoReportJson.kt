fdbebe6ddb0d147474c89349e894a07d0fcc3fa1
jaydeep <jaydeepk@gmail.com>
2023-10-05 11:34:59 +0530

    Implemented a CentralContractRepoReportCommand to generate a report (json) which lists all the operations present in all specifications in the central contract repo.

---
+++ core/src/main/kotlin/in/specmatic/reports/CentralContractRepoReportJson.kt
@@ -0,0 +1,22 @@
+package `in`.specmatic.reports
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CentralContractRepoReportJson(
+    val specifications: List<SpecificationRow>
+)
+
+@Serializable
+data class SpecificationRow(
+    val specification: String,
+    val serviceType: String?,
+    val operations: List<SpecificationOperation>
+)
+
+@Serializable
+data class SpecificationOperation(
+    val path: String,
+    val method: String,
+    val responseCode: Int
+)

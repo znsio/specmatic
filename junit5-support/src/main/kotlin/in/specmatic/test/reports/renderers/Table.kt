2280d5d767f768699cb7217a8c76bfe08f42ad7c
Joel Rosario <cirquitz@gmail.com>
2023-11-04 18:45:25 +0530

    Added a Table class to handle tables

---
+++ junit5-support/src/main/kotlin/in/specmatic/test/reports/renderers/Table.kt
@@ -0,0 +1,40 @@
+package `in`.specmatic.test.reports.renderers
+
+class Table(private val title: String, private val header: List<String>, private val rows: List<List<String>>, private val summary: String?) {
+    fun asPrintableString(): String {
+        val columnCount = (listOf(header.size) + rows.map { it.size }).max()
+        val columnWidths = (0 until columnCount).map { columnIndex ->
+            (listOf(header.getOrNull(columnIndex)?.length ?: 0) + rows.map { it.getOrNull(columnIndex)?.length ?: 0 }).max()
+        }
+
+        val headersWithEqualizedLengths = header + (0 until (columnCount - header.size)).map { "" }
+        val tableHeader = headersWithEqualizedLengths.mapIndexed { index, column ->
+            "%-${columnWidths[index]}s".format(column)
+        }.joinToString(" | ", prefix = "| ", postfix = " |")
+
+        val headerSeparator = columnWidths.joinToString("+", prefix = "|", postfix = "|") { "-".repeat(it + 2) }
+
+        val headerTitleSize = tableHeader.length - 4
+        val tableTitle = "| ${"%-${headerTitleSize}s".format(title)} |"
+        val titleSeparator = "|-${"-".repeat(headerTitleSize)}-|"
+
+        val summaryRowFormatter = "%-${headerTitleSize}s"
+        val summaryRow = "| ${summaryRowFormatter.format(summary)} |"
+
+        val header: List<String> = listOf(titleSeparator, tableTitle, titleSeparator, tableHeader, headerSeparator)
+        val rowsWithEqualizedLength = rows.map { row ->
+            row + (0 until (columnCount - row.size)).map { "" }
+        }
+        val tableBody: List<String> = rowsWithEqualizedLength.map { row ->
+            row.mapIndexed { index, column ->
+                "%-${columnWidths[index]}s".format(column)
+            }.joinToString(" | ", prefix = "| ", postfix = " |")
+        }
+        val footer: List<String> = listOf(titleSeparator, summaryRow, titleSeparator)
+
+        val coveredAPIsTable =  (header + tableBody + footer).joinToString(System.lineSeparator())
+
+        return coveredAPIsTable
+
+    }
+}
---
+++ junit5-support/src/test/kotlin/in/specmatic/test/reports/renderers/TableTest.kt
@@ -0,0 +1,67 @@
+package `in`.specmatic.test.reports.renderers
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class TableTest {
+    @Test
+    fun `should generate a table given title summary and rows`() {
+        val table = Table("Prices", listOf("Region", "Apples", "Oranges"), listOf(listOf("North", "10", "20"), listOf("South", "100", "120")), "Summary")
+        assertThat(table.asPrintableString()).isEqualTo(
+            """
+                |---------------------------|
+                | Prices                    |
+                |---------------------------|
+                | Region | Apples | Oranges |
+                |--------+--------+---------|
+                | North  | 10     | 20      |
+                | South  | 100    | 120     |
+                |---------------------------|
+                | Summary                   |
+                |---------------------------|
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `should handle columns of differing lengths`() {
+        val table = Table("Prices", listOf("Region", "Apples", "Oranges", "Pears"), listOf(listOf("North", "10", "20"), listOf("South", "100", "120"), listOf("East", "100", "50", "300")), "Summary")
+        assertThat(table.asPrintableString()).isEqualTo(
+            """
+                |-----------------------------------|
+                | Prices                            |
+                |-----------------------------------|
+                | Region | Apples | Oranges | Pears |
+                |--------+--------+---------+-------|
+                | North  | 10     | 20      |       |
+                | South  | 100    | 120     |       |
+                | East   | 100    | 50      | 300   |
+                |-----------------------------------|
+                | Summary                           |
+                |-----------------------------------|
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `should handle more row columns than header columns`() {
+        val table = Table("Prices", listOf("Region", "Apples", "Oranges"), listOf(listOf("North", "10", "20"), listOf("South", "100", "120"), listOf("East", "100", "50", "300")), "Summary")
+        assertThat(table.asPrintableString()).isEqualTo(
+            """
+                |---------------------------------|
+                | Prices                          |
+                |---------------------------------|
+                | Region | Apples | Oranges |     |
+                |--------+--------+---------+-----|
+                | North  | 10     | 20      |     |
+                | South  | 100    | 120     |     |
+                | East   | 100    | 50      | 300 |
+                |---------------------------------|
+                | Summary                         |
+                |---------------------------------|
+            """.trimIndent()
+        )
+    }
+}

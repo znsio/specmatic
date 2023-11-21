f83be1f95aeff5af0022b48c7f31c37e592e6170
Joel Rosario <cirquitz@gmail.com>
2023-11-21 16:21:44 +0530

    Added progress update

--- junit5-support/src/main/kotlin/in/specmatic/test/SpecmaticJUnitSupport.kt
+++ junit5-support/src/main/kotlin/in/specmatic/test/SpecmaticJUnitSupport.kt
@@ -202,6 +202,7 @@ open class SpecmaticJUnitSupport {
         }
 
         var checkedAPIs = false
+        var totalRun = 0
 
         return testScenarios.map { testScenario ->
             DynamicTest.dynamicTest(testScenario.testDescription()) {
@@ -235,9 +236,24 @@ open class SpecmaticJUnitSupport {
                 } catch(e: Throwable) {
                     openApiCoverageReportInput.addTestReportRecords(testScenario.testResultRecord(Result.Failure(exceptionCauseMessage(e))))
                     throw e
+                } finally {
+                    totalRun += 1
+                    logger.newLine()
+                    logger.log(progressUpdate(totalRun, testScenarios.size))
                 }
             }
         }.toList()
+    }
+
+    private fun progressUpdate(totalTestsRun: Int, countOfTests: Int): String {
+        return "Tests run: $totalTestsRun/$countOfTests (${percentage(totalTestsRun, countOfTests)}%)"
+    }
+
+    private fun percentage(totalTestsRun: Int, countOfTests: Int): String {
+        return if(countOfTests == 0)
+            "0"
+        else
+            ((totalTestsRun.toDouble() / countOfTests.toDouble()) * 100).toInt().toString()
     }
 
     private fun getSpecmaticJson(): SpecmaticConfigJson? {

c38ff84f9b127e3b8eab1d0f34245069e7293aa4
Joel Rosario <cirquitz@gmail.com>
2023-11-26 18:57:43 +0530

    Adds test for generative tests same key in different areas of request

---
+++ core/src/test/kotlin/in/specmatic/conversions/GenerativeTests.kt
@@ -0,0 +1,104 @@
+package `in`.specmatic.conversions
+
+import `in`.specmatic.core.Flags
+import `in`.specmatic.core.HttpRequest
+import `in`.specmatic.core.HttpResponse
+import `in`.specmatic.core.pattern.ContractException
+import `in`.specmatic.core.value.JSONObjectValue
+import `in`.specmatic.core.value.Value
+import `in`.specmatic.test.TestExecutor
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
+import org.junit.jupiter.api.Test
+
+class GenerativeTests {
+    @Test
+    fun `handle request payloads with the same key in different parts of the payload with different types`() {
+        val feature = OpenApiSpecification.fromYAML(
+            """
+            openapi: 3.0.0
+            info:
+              title: Addresses
+              version: 1.0.0
+            paths:
+              /addresses:
+                post:
+                  requestBody:
+                    content:
+                      application/json:
+                        examples:
+                          SUCCESS:
+                            value:
+                              person:
+                                address:
+                                  building: 1
+                              company:
+                                address:
+                                  building: "Bldg no 1"
+                        schema:
+                          type: object
+                          required:
+                            - person
+                            - company
+                          properties:
+                            person:
+                              type: object
+                              required:
+                                - address
+                              properties:
+                                address:
+                                  type: object
+                                  required:
+                                    - building
+                                  properties:
+                                    building:
+                                      type: integer
+                            company:
+                              type: object
+                              required:
+                                - address
+                              properties:
+                                address:
+                                  type: object
+                                  required:
+                                    - building
+                                  properties:
+                                    building:
+                                      type: string
+                  responses:
+                    '200':
+                      description: OK
+                      content:
+                        text/plain:
+                          schema:
+                            type: string
+                          examples:
+                            SUCCESS:
+                              value: OK
+        """.trimIndent(), "").toFeature()
+
+        val building = mutableListOf<Any>()
+
+        try {
+            System.setProperty(Flags.onlyPositive, "true")
+            val results = feature.copy(generativeTestingEnabled = true).executeTests(object : TestExecutor {
+                override fun execute(request: HttpRequest): HttpResponse {
+                    val body = request.body as JSONObjectValue
+                    building.add(body.findFirstChildByPath("person.address.building")!!.toStringValue())
+                    return HttpResponse.OK("OK")
+                }
+
+                override fun setServerState(serverState: Map<String, Value>) {
+                }
+            })
+
+            println(results.report())
+
+            assertThat(building).contains("1", "Bldg no 1")
+        } catch(e: ContractException) {
+            fail("Should not have got this error:\n${e.report()}")
+        } finally {
+            System.clearProperty(Flags.onlyPositive)
+        }
+    }
+}

7f0c0c82531a69719e840988f4619808954f70b3
jaydeep <jaydeepk@gmail.com>
2023-12-20 10:57:28 +0530

    Refactored the retrieval of security tokens for Bearer and API Key security schemes.

--- core/src/main/kotlin/in/specmatic/conversions/SecurityToken.kt
+++ core/src/main/kotlin/in/specmatic/conversions/SecurityToken.kt
@@ -1,5 +1,25 @@
 package `in`.specmatic.conversions
 
-interface SecurityToken {
-    fun resolve(): String?
+import `in`.specmatic.core.APIKeySecuritySchemeConfiguration
+import `in`.specmatic.core.SecuritySchemeConfiguration
+import `in`.specmatic.core.SecuritySchemeWithOAuthToken
+
+fun getSecurityTokenForBearerScheme(
+    securitySchemeConfiguration: SecuritySchemeConfiguration?,
+    environmentVariable: String,
+    environment: Environment
+): String? {
+    return environment.getEnvironmentVariable(environmentVariable) ?: securitySchemeConfiguration?.let {
+        (it as SecuritySchemeWithOAuthToken).token
+    }
+}
+
+fun getSecurityTokenForApiKeyScheme(
+    securitySchemeConfiguration: SecuritySchemeConfiguration?,
+    environmentVariable: String,
+    environment: Environment
+): String? {
+    return environment.getEnvironmentVariable(environmentVariable) ?: securitySchemeConfiguration?.let {
+        (it as APIKeySecuritySchemeConfiguration).value
+    }
 }

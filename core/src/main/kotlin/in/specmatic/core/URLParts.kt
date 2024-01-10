2b16e23c674549f963435b0861c399225e847cfe
Hari Krishnan <harikrishnan83@gmail.com>
2024-01-10 13:21:23 +0530

    extracted URLParts to separate class, added unit tests

---
+++ core/src/main/kotlin/in/specmatic/core/URLParts.kt
@@ -0,0 +1,37 @@
+package `in`.specmatic.core
+
+private const val MIN_URL_PARTS_WHEN_PATH_EXISTS = 4
+private const val QUERY_PARAM_START_CHAR = '?'
+
+class URLParts(url: String) {
+    private val queryStartIndex = url.indexOf(QUERY_PARAM_START_CHAR)
+    private val baseUrl = if (queryStartIndex != -1) url.substring(0, queryStartIndex) else url
+
+    val parts = baseUrl.split("/", limit = MIN_URL_PARTS_WHEN_PATH_EXISTS)
+
+    private val queryOnwards = if (queryStartIndex != -1) url.substring(queryStartIndex) else ""
+
+    fun withEncodedPathSegments(): String {
+        if(noPathInURL())
+            return baseUrl
+
+        val (scheme, _, authority, path) = parts
+
+        val escapedPath = escapeSpaceInPath(path)
+
+        return "$scheme//$authority/$escapedPath$queryOnwards"
+    }
+
+    fun withDecodedPathSegments(): String {
+        if(noPathInURL())
+            return baseUrl
+
+        val (scheme, _, authority, path) = parts
+
+        val escapedPath = decodePath(path)
+
+        return "$scheme//$authority/$escapedPath$queryOnwards"
+    }
+
+    private fun noPathInURL() = parts.size < MIN_URL_PARTS_WHEN_PATH_EXISTS
+}

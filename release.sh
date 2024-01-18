e061820971415c7c1e802618ed8f7ec5ebdb7f66
Joel Rosario <cirquitz@gmail.com>
2024-01-18 17:39:55 +0530

    Updated the release script to check the version of Specmatic in specmatic-documentation's config

--- release.sh
+++ release.sh
@@ -79,6 +79,33 @@ else
 	echo √ Specified version matches ./version.properties
 fi
 
+if [ ! -d ../specmatic-documentation ]
+then
+	echo
+	echo Error: cannot find the specmatic-documentation checkout. Make sure that git@github.com:znsio/specmatic-documentation.git is checked out at ../specmatic-documentation
+	exit 1
+fi
+
+if [ ! -f ../specmatic-documentation/_config.yml ]
+then
+	echo
+	echo Error: cannot find specmatic-documentation/_config.yml, need to check this file to ensure that it is in sync with the latest version of Specmatic.
+	exit 1
+fi
+
+DOCUMENTATION_VERSION=$(cat ../specmatic-documentation/_config.yml | grep latest_release | cut -d ' ' -f 2)
+
+if [ "$DOCUMENTATION_VERSION" != $ACTUAL_VERSION ]
+then
+  echo
+  echo Error: The version in specmatic-documentation/_config.yml is $DOCUMENTATION_VERSION, but the version in version.properties is $ACTUAL_VERSION.
+  echo
+  echo To fix this, update the version in specmatic-documentation/_config.yml to $ACTUAL_VERSION, commit and push the change, and try again.
+  exit 1
+else
+  echo √ Version in specmatic-documentation/_config.yml matches version in version.properties
+fi
+
 echo
 
 

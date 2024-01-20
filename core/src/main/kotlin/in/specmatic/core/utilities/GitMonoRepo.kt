4ff8af0dbb5a3ba4009923f11ccc5078d8ccabd5
Joel Rosario <cirquitz@gmail.com>
2024-01-20 17:53:34 +0530

    Implemented local file system

---
+++ core/src/main/kotlin/in/specmatic/core/utilities/GitMonoRepo.kt
@@ -0,0 +1,47 @@
+package `in`.specmatic.core.utilities
+
+import `in`.specmatic.core.git.SystemGit
+import java.io.File
+
+data class GitMonoRepo(override val testContracts: List<String>, override val stubContracts: List<String>,
+                       override val type: String?) : ContractSource {
+    override fun pathDescriptor(path: String): String = path
+    override fun install(workingDirectory: File) {
+        println("Checking list of mono repo paths...")
+
+        val contracts = testContracts + stubContracts
+
+        for (path in contracts) {
+            val existenceMessage = when {
+                File(path).exists() -> "$path exists"
+                else -> "$path NOT FOUND!"
+            }
+
+            println(existenceMessage)
+        }
+    }
+
+    override fun directoryRelativeTo(workingDirectory: File) = workingDirectory.resolve(gitRootDir())
+
+    override fun getLatest(sourceGit: SystemGit) {
+        // In mono repos, we can't pull latest arbitrarily
+    }
+
+    override fun pushUpdates(sourceGit: SystemGit) {
+        // In mono repos, we can't push arbitrarily
+    }
+
+    override fun loadContracts(selector: ContractsSelectorPredicate, workingDirectory: String, configFilePath: String): List<ContractPathData> {
+        val monoRepoBaseDir = File(SystemGit().gitRoot())
+        val configFileLocation = File(configFilePath).absoluteFile.parentFile
+
+        return selector.select(this).map {
+            ContractPathData(
+                monoRepoBaseDir.canonicalPath,
+                configFileLocation.resolve(it).canonicalPath,
+                provider = type,
+                specificationPath = it
+            )
+        }
+    }
+}
---
+++ core/src/main/kotlin/in/specmatic/core/utilities/LocalFileSystemSource.kt
@@ -0,0 +1,51 @@
+package `in`.specmatic.core.utilities
+
+import `in`.specmatic.core.git.SystemGit
+import `in`.specmatic.core.log.logger
+import java.io.File
+
+class LocalFileSystemSource(
+    val directory: String,
+    override val testContracts: List<String>,
+    override val stubContracts: List<String>
+) : ContractSource {
+    override val type = "filesystem"
+
+    override fun pathDescriptor(path: String): String = path
+
+    override fun install(workingDirectory: File) {
+        logger.log("No installation needed as this source is a directory on the local file system")
+    }
+
+    override fun directoryRelativeTo(workingDirectory: File): File {
+        if(File(directory).isAbsolute)
+            return File(directory)
+
+        return workingDirectory.resolve(directory)
+    }
+
+    override fun getLatest(sourceGit: SystemGit) {
+        logger.log("No need to get latest as this source is a directory on the local file system")
+    }
+
+    override fun pushUpdates(sourceGit: SystemGit) {
+        logger.log("No need to push updates as this source is a directory on the local file system")
+    }
+
+    override fun loadContracts(
+        selector: ContractsSelectorPredicate,
+        workingDirectory: String,
+        configFilePath: String
+    ): List<ContractPathData> {
+        return selector.select(this).map {
+            val resolvedPath = File(directory).resolve(it)
+
+            ContractPathData(
+                directory,
+                resolvedPath.path,
+                provider = type,
+                specificationPath = resolvedPath.canonicalPath,
+            )
+        }
+    }
+}

7b1c82042efa3dd88680f8daf0d00bc6b4972ed8
Joel Rosario <cirquitz@gmail.com>
2024-02-22 12:00:10 +0530

    Improved error message for optional body

--- core/src/main/kotlin/in/specmatic/conversions/OptionalBodyPattern.kt
+++ core/src/main/kotlin/in/specmatic/conversions/OptionalBodyPattern.kt
@@ -1,13 +1,31 @@
 package `in`.specmatic.conversions
 
 import `in`.specmatic.core.NoBodyPattern
+import `in`.specmatic.core.Resolver
+import `in`.specmatic.core.Result
 import `in`.specmatic.core.pattern.AnyPattern
 import `in`.specmatic.core.pattern.Pattern
+import `in`.specmatic.core.value.Value
 
-class OptionalBodyPattern(override val pattern: AnyPattern) : Pattern by pattern {
+data class OptionalBodyPattern(override val pattern: AnyPattern, private val bodyPattern: Pattern) : Pattern by pattern {
     companion object {
         fun fromPattern(bodyPattern: Pattern): OptionalBodyPattern {
-            return OptionalBodyPattern(AnyPattern(listOf(bodyPattern, NoBodyPattern)))
+            return OptionalBodyPattern(AnyPattern(listOf(bodyPattern, NoBodyPattern)), bodyPattern)
         }
+    }
+
+
+    override fun matches(sampleData: Value?, resolver: Resolver): Result {
+        val bodyPatternMatchResult = bodyPattern.matches(sampleData, resolver)
+
+        if(bodyPatternMatchResult is Result.Success)
+            return bodyPatternMatchResult
+
+        val nobodyPatternMatchResult = NoBodyPattern.matches(sampleData, resolver)
+
+        if(nobodyPatternMatchResult is Result.Success)
+            return nobodyPatternMatchResult
+
+        return bodyPatternMatchResult
     }
 }
---
+++ core/src/test/kotlin/in/specmatic/conversions/OptionalBodyPatternTest.kt
@@ -0,0 +1,18 @@
+package `in`.specmatic.conversions
+
+import `in`.specmatic.core.Resolver
+import `in`.specmatic.core.pattern.NumberPattern
+import `in`.specmatic.core.value.StringValue
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class OptionalBodyPatternTest {
+    @Test
+    fun `optional body error match`() {
+        val body = OptionalBodyPattern.fromPattern(NumberPattern())
+
+        val matchResult = body.matches(StringValue("abc"), Resolver())
+        assertThat(matchResult.reportString().trim()).isEqualTo("Expected number, actual was \"abc\"")
+    }
+}

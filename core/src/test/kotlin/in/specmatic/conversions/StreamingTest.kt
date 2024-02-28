b11b02852af2ce61eb0ca04da70b9b4e9c3f18ef
Joel Rosario <cirquitz@gmail.com>
2024-02-28 12:38:41 +0530

    Added a test to ensure that streaming really works

---
+++ core/src/test/kotlin/in/specmatic/conversions/StreamingTest.kt
@@ -0,0 +1,45 @@
+package `in`.specmatic.conversions
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StreamingTest {
+    @Test
+    fun `should stream tests`() {
+        val parameters = (1..30).joinToString("") {
+"""        - name: param$it
+          in: query
+          schema:
+            type: string
+"""
+        }
+
+        val specWith30QueryParams = """
+openapi: 3.0.0
+info:
+  title: "Test API"
+  version: "1.0.0"
+paths:
+  /test:
+    get:
+      parameters:
+$parameters
+      responses:
+        '200':
+          description: "A simple test"
+          content:
+            text/plain:
+              schema:
+                type: string
+""".trimIndent()
+
+        val feature = OpenApiSpecification.fromYAML(specWith30QueryParams, "").toFeature()
+
+        val streamOfTestsWithOverABillionTests = feature.generateContractTests(emptyList())
+        val first100TestsFromTheStreamingTest = streamOfTestsWithOverABillionTests.take(100)
+
+        assertThat(first100TestsFromTheStreamingTest.toList()).hasSize(100)
+    }
+
+
+}

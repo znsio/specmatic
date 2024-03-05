9112c1f24f1f4dbe97a47e265181cb61906fd6fb
Joel Rosario <cirquitz@gmail.com>
2024-03-05 15:05:38 +0530

    Added a test for mandatory parameter with no example

--- core/src/test/kotlin/integration_tests/OmittedParameterExamples.kt
+++ core/src/test/kotlin/integration_tests/OmittedParameterExamples.kt
@@ -92,6 +92,144 @@ components:
     }
 
     @Test
+    fun `mandatory query param without example should be generated`() {
+        val productSpec = OpenApiSpecification.fromYAML("""
+openapi: 3.0.3
+info:
+  title: Product Search Service
+  description: API for searching product details
+  version: 1.0.0
+servers:
+  - url: 'https://localhost:8080'
+paths:
+  /product/search:
+    get:
+      summary: Search for product details
+      parameters:
+        - name: productCategory
+          in: query
+          description: Category of the product to search for
+          required: true
+          schema:
+            type: string
+        - name: priceRange
+          in: query
+          description: Price range of the product to search for
+          required: false
+          schema:
+            type: string
+          examples:
+            PRODUCT_SEARCH:
+              value: "1000-2000"
+      responses:
+        200:
+          description: Successful operation
+          content:
+            application/json:
+              schema:
+                type: array
+                items:
+                  ${"$"}ref: '#/components/schemas/Product'
+              examples:
+                PRODUCT_SEARCH:
+                    value:
+                      - id: 1
+                        name: "Product 1"
+                      - id: 2
+                        name: "Product 2"
+components:
+  schemas:
+    Product:
+      type: object
+      properties:
+        id:
+          type: integer
+        name:
+          type: string
+        """.trimIndent(), "").toFeature()
+
+        val optionalQueryParam = "productName"
+
+        val results = productSpec.executeTests(object : TestExecutor {
+            override fun execute(request: HttpRequest): HttpResponse {
+                assertThat(request.queryParams.keys).contains("productCategory")
+                return HttpResponse(200, body = parsedJSONArray("""[{"id": 1, "name": "Product 1"}, {"id": 2, "name": "Product 2"}]"""))
+            }
+        })
+
+        assertThat(results.success()).isTrue()
+        assertThat(results.successCount).isEqualTo(1)
+    }
+
+    @Test
+    fun `mandatory header without example should be generated`() {
+        val productSpec = OpenApiSpecification.fromYAML("""
+openapi: 3.0.3
+info:
+  title: Product Search Service
+  description: API for searching product details
+  version: 1.0.0
+servers:
+  - url: 'https://localhost:8080'
+paths:
+  /product/search:
+    get:
+      summary: Search for product details
+      parameters:
+        - name: productCategory
+          in: header
+          description: Category of the product to search for
+          required: true
+          schema:
+            type: string
+        - name: priceRange
+          in: header
+          description: Price range of the product to search for
+          required: false
+          schema:
+            type: string
+          examples:
+            PRODUCT_SEARCH:
+              value: "1000-2000"
+      responses:
+        200:
+          description: Successful operation
+          content:
+            application/json:
+              schema:
+                type: array
+                items:
+                  ${"$"}ref: '#/components/schemas/Product'
+              examples:
+                PRODUCT_SEARCH:
+                    value:
+                      - id: 1
+                        name: "Product 1"
+                      - id: 2
+                        name: "Product 2"
+components:
+  schemas:
+    Product:
+      type: object
+      properties:
+        id:
+          type: integer
+        name:
+          type: string
+        """.trimIndent(), "").toFeature()
+
+        val results = productSpec.executeTests(object : TestExecutor {
+            override fun execute(request: HttpRequest): HttpResponse {
+                assertThat(request.headers.keys).contains("productCategory")
+                return HttpResponse(200, body = parsedJSONArray("""[{"id": 1, "name": "Product 1"}, {"id": 2, "name": "Product 2"}]"""))
+            }
+        })
+
+        assertThat(results.success()).isTrue()
+        assertThat(results.successCount).isEqualTo(1)
+    }
+
+    @Test
     fun `omitted optional query params should be sent when generative tests are switch on`() {
         val productSpec = OpenApiSpecification.fromYAML("""
 openapi: 3.0.3

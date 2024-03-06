6f8940c2d61923f920ed71a58689b67a0a3f948e
Joel Rosario <cirquitz@gmail.com>
2024-03-06 14:55:49 +0530

    Check the Content-Type when matching request and response headers

--- core/src/main/kotlin/in/specmatic/core/HttpHeadersPattern.kt
+++ core/src/main/kotlin/in/specmatic/core/HttpHeadersPattern.kt
@@ -33,9 +33,20 @@ data class HttpHeadersPattern(
     private fun matchEach(parameters: Pair<Map<String, String>, Resolver>): MatchingResult<Pair<Map<String, String>, Resolver>> {
         val (headers, resolver) = parameters
 
+        val contentTypeHeaderValue = headers["Content-Type"]
+        if(contentType != null && contentTypeHeaderValue != null && contentType != contentTypeHeaderValue)
+            return MatchFailure(
+                Result.Failure(
+                    "Content-Type header mismatch, expected \"$contentType\", found \"$contentTypeHeaderValue\"",
+                    breadCrumb = "Content-Type",
+                    failureReason = FailureReason.ContentTypeMismatch
+                )
+            )
+
+
         val headersWithRelevantKeys = when {
             ancestorHeaders != null -> withoutIgnorableHeaders(headers, ancestorHeaders)
-            else -> withoutContentTypeGeneratedByQontract(headers, pattern)
+            else -> withoutContentTypeGeneratedBySpecmatic(headers, pattern)
         }
 
         val keyErrors: List<KeyError> =
@@ -115,7 +126,7 @@ data class HttpHeadersPattern(
         }
     }
 
-    private fun withoutContentTypeGeneratedByQontract(
+    private fun withoutContentTypeGeneratedBySpecmatic(
         headers: Map<String, String>,
         pattern: Map<String, Pattern>
     ): Map<String, String> {

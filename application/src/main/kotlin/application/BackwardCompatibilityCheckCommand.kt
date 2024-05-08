f560f59115fbd4c5a8705ddfebe022a2224bc37c
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-05-08 17:32:37 +0530

    Add logic to run backward compatibility check on every changed file and files that refer to changed schema file

--- application/src/main/kotlin/application/BackwardCompatibilityCheckCommand.kt
+++ application/src/main/kotlin/application/BackwardCompatibilityCheckCommand.kt
@@ -2,52 +2,76 @@ package application
 
 import `in`.specmatic.core.CONTRACT_EXTENSIONS
 import `in`.specmatic.core.git.GitCommand
-import `in`.specmatic.core.git.SystemGit
 import org.junit.platform.launcher.Launcher
 import org.springframework.beans.factory.annotation.Autowired
-import picocli.CommandLine.*
+import org.springframework.stereotype.Component
+import picocli.CommandLine.Command
 import java.io.File
 import java.util.concurrent.Callable
 
+@Component
 @Command(name = "backwardCompatibilityCheck",
         mixinStandardHelpOptions = true,
         description = ["Checks backward compatibility of a directory across the current HEAD and the main branch"])
-class BackwardCompatibilityCheckCommand : Callable<Int> {
-//    @Autowired
-//    lateinit var gitCommand: GitCommand
-
-    val gitCommand: GitCommand = SystemGit()
-
-    @Autowired
-    lateinit var fileOperations: FileOperations
-
-    @Autowired
-    lateinit var junitLauncher: Launcher
+class BackwardCompatibilityCheckCommand(
+    private val gitCommand: GitCommand,
+    private val gitCompatibleCommand: GitCompatibleCommand,
+    private val fileOperations: FileOperations,
+    private val junitLauncher: Launcher,
+) : Callable<Int> {
 
     override fun call(): Int {
-        val filesChangedInCurrentBranch: Set<String> = gitCommand.getFilesChangeInCurrentBranch().filter { File(it).extension in CONTRACT_EXTENSIONS }.toSet()
+        val filesChangedInCurrentBranch: Set<String> = gitCommand.getFilesChangeInCurrentBranch().filter {
+            File(it).extension in CONTRACT_EXTENSIONS
+        }.toSet()
 
-        val changedSchemaFiles: Set<String> = filesChangedInCurrentBranch.filterNot { File(it).readText().lines().any { it.matches(Regex("paths:")) } }.toSet()
-        val schemaFileBaseNames = changedSchemaFiles.map { File(it).name }
+        val changedSchemaFiles: Set<String> = filesChangedInCurrentBranch.filter {
+            isSchemaFile(File(it))
+        }.toSet()
 
-        val allFiles = File(".").walk().toList().filterNot { ".git" in it.path }
+        val filesToCheck: Set<String> =
+            filesChangedInCurrentBranch - changedSchemaFiles + filesReferringToChangedSchemaFiles(changedSchemaFiles)
 
-        val filesReferringToChangedSchemaFiles = allFiles.filter {
+        logFilesToBeCheckedForBackwardCompatibility(filesToCheck)
+        runBackwardCompatibilityCheckFor(filesToCheck)
+
+        return 0
+    }
+
+    private fun runBackwardCompatibilityCheckFor(files: Set<String>) {
+        files.forEachIndexed { index, it ->
+            println("[${index.inc()}] Running the check for $it:")
+            gitCompatibleCommand.file(it, "", false)
+            println("-".repeat(20))
+            println()
+        }
+    }
+
+    private fun logFilesToBeCheckedForBackwardCompatibility(files : Set<String>) {
+        println("Checking backward compatibility of the following files: ")
+        files.forEach { println(it) }
+        println("-".repeat(20))
+        println()
+    }
+
+    private fun filesReferringToChangedSchemaFiles(schemaFiles : Set<String>): Set<String> {
+        if(schemaFiles.isEmpty()) return emptySet()
+
+        val schemaFileBaseNames = schemaFiles.map { File(it).name }
+        return allFiles().filter {
             it.readText().let { specContent ->
                 schemaFileBaseNames.any { schemaFileBaseName -> schemaFileBaseName in specContent }
             }
         }.map { it.path }.toSet()
+    }
 
-        val filesToCheck: Set<String> = (filesChangedInCurrentBranch - changedSchemaFiles) + filesReferringToChangedSchemaFiles
+    private fun isSchemaFile(file: File): Boolean {
+        return file.readText().lines().any { it.matches(Regex("paths:")) }.not()
+    }
 
-        println("Checking backward compatibility of the following files: ")
-
-        filesToCheck.forEach {
-            print(it)
-        }
-
-        // Pull logic from CompatibleCommand to check each file in filesToCheck
-
-        return 0
+    private fun allFiles(): List<File> {
+        return File(".").walk().toList().filterNot {
+            ".git" in it.path
+        }.filter { it.isFile }
     }
 }

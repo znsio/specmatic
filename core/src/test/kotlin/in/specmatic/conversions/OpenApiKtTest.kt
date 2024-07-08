40317b033ce89f86193967bba646225091b80048
Joel Rosario <cirquitz@gmail.com>
2024-07-08 15:54:36 +0530

    Implemented positive and negative tests generation support for lengths in StringPattern

--- core/src/test/kotlin/in/specmatic/conversions/OpenApiKtTest.kt
+++ core/src/test/kotlin/in/specmatic/conversions/OpenApiKtTest.kt
@@ -1323,7 +1323,7 @@ Background:
         assertThat(flags["/pets/0 GET executed"]).isEqualTo(1)
         assertThat(flags.keys.filter { it.matches(Regex("""/pets/\d+ GET executed""")) }.size).isEqualTo(2)
         assertThat(flags.keys.any { it.matches(Regex("""/pets/\d+ DELETE executed""")) }).isNotNull
-        assertThat(flags.filter {(path, _) -> path.matches(Regex("""/pets/\d+ PATCH executed""")) }.values.sum()).isEqualTo(7)
+        assertThat(flags.filter {(path, _) -> path.matches(Regex("""/pets/\d+ PATCH executed""")) }.values.sum()).isEqualTo(21)
         assertTrue(results.success(), results.report())
     }
 

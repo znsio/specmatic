e1565c44352db033e1e38b1b8e8d7f479959c85d
Sufiyan <StarKhan6368@gmail.com>
2024-08-09 17:20:52 +0530

    Add tests for timeout scenarios in Proxy.

--- core/src/test/kotlin/io/specmatic/proxy/ProxyTest.kt
+++ core/src/test/kotlin/io/specmatic/proxy/ProxyTest.kt
@@ -1,22 +1,21 @@
 package io.specmatic.proxy
 
 import io.specmatic.conversions.OpenApiSpecification
-import io.specmatic.core.HttpRequest
-import io.specmatic.core.HttpResponse
 import io.specmatic.core.YAML
 import io.specmatic.core.parseGherkinStringToFeature
 import io.specmatic.core.pattern.parsedJSON
 import io.specmatic.core.pattern.parsedJSONObject
 import io.specmatic.core.value.JSONObjectValue
-import io.specmatic.core.value.StringValue
-import io.specmatic.mock.ScenarioStub
 import io.specmatic.stub.HttpStub
 import io.ktor.http.*
+import io.specmatic.mock.DELAY_IN_SECONDS
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.postForEntity
 import java.io.File
 import java.net.InetSocketAddress
 
@@ -213,6 +212,69 @@ internal class ProxyTest {
 
         HttpStub(OpenApiSpecification.fromYAML(fakeFileWriter.receivedContract!!, "").toFeature()).use { stub ->
 
+        }
+    }
+
+    @Test
+    fun `should not timeout if custom timeout is greater than default with slow response`() {
+        HttpStub(simpleFeature).use { fake ->
+            val expectation = """ {
+                "http-request": {
+                    "method": "POST",
+                    "path": "/",
+                    "body": "10"
+                },
+                "http-response": {
+                    "status": 200,
+                    "body": "100"
+                },
+                "$DELAY_IN_SECONDS": 7
+            }""".trimIndent()
+
+            val stubResponse =  RestTemplate().postForEntity<String>(fake.endPoint + "/_specmatic/expectations", expectation)
+            assertThat(stubResponse.statusCode.value()).isEqualTo(200)
+
+            Proxy(host = "localhost", port = 9001, "", fakeFileWriter, timeoutInMilliseconds = 8000).use {
+                val restProxy = java.net.Proxy(java.net.Proxy.Type.HTTP, InetSocketAddress("localhost", 9001))
+                val requestFactory = SimpleClientHttpRequestFactory()
+                requestFactory.setProxy(restProxy)
+                val client = RestTemplate(requestFactory)
+                val response = client.postForEntity("http://localhost:9000/", "10", String::class.java)
+
+                assertThat(response.statusCodeValue).isEqualTo(200)
+                assertThatNoException().isThrownBy { response.body!!.toInt() }
+            }
+        }
+    }
+
+    @Test
+    fun `should timeout if custom timeout is less than default with slow response`() {
+        HttpStub(simpleFeature).use { fake ->
+            val expectation = """ {
+                "http-request": {
+                    "method": "POST",
+                    "path": "/",
+                    "body": "10"
+                },
+                "http-response": {
+                    "status": 200,
+                    "body": "100"
+                },
+                "$DELAY_IN_SECONDS": 5
+            }""".trimIndent()
+
+            val stubResponse =  RestTemplate().postForEntity<String>(fake.endPoint + "/_specmatic/expectations", expectation)
+            assertThat(stubResponse.statusCode.value()).isEqualTo(200)
+
+            assertThrows<Exception>{
+                Proxy(host = "localhost", port = 9001, "", fakeFileWriter, timeoutInMilliseconds = 2000).use {
+                    val restProxy = java.net.Proxy(java.net.Proxy.Type.HTTP, InetSocketAddress("localhost", 9001))
+                    val requestFactory = SimpleClientHttpRequestFactory()
+                    requestFactory.setProxy(restProxy)
+                    val client = RestTemplate(requestFactory)
+                    client.postForEntity("http://localhost:9000/", "10", String::class.java)
+                }
+            }
         }
     }
 }

3253f95c18eb714c356c0051b25a45b771160d9b
Sufiyan <StarKhan6368@gmail.com>
2024-08-23 09:09:39 +0530

    Add tests, ensure coverage percentage calculation.
    - Calculation should be done on exercised count
      not on remarks.

--- junit5-support/src/test/kotlin/io/specmatic/test/OpenApiCoverageConsoleReportTest.kt
+++ junit5-support/src/test/kotlin/io/specmatic/test/OpenApiCoverageConsoleReportTest.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test
 class OpenApiCoverageConsoleReportTest {
 
     @Test
-    fun `test calculates total percentage based on number of covered endpoints`() {
+    fun `test calculates total percentage based on number of exercised endpoints`() {
         val rows = listOf(
             OpenApiCoverageConsoleRow("GET", "/route1", 200, 1, 50, Remarks.Covered),
             OpenApiCoverageConsoleRow("POST", "/route1", 200, 0, 0, Remarks.Missed),
@@ -23,5 +23,26 @@ class OpenApiCoverageConsoleReportTest {
         val coverageReport = OpenAPICoverageConsoleReport(rows, emptyList(), totalEndpointsCount = 3, missedEndpointsCount = 0, notImplementedAPICount = 0, partiallyMissedEndpointsCount = 1, partiallyNotImplementedAPICount = 0)
 
         assertThat(coverageReport.totalCoveragePercentage).isEqualTo(28)
+    }
+
+    @Test
+    fun `should calculate overall coverage percentage based on exercised endpoints with WIP`() {
+        val rows = listOf(
+            OpenApiCoverageConsoleRow("GET", "/route1", 200, 2, 100, Remarks.Covered),
+            OpenApiCoverageConsoleRow("GET", "/route1", 400, 2, 100, Remarks.Covered),
+            OpenApiCoverageConsoleRow("GET", "/route1", 503, 1, 100, Remarks.Covered),
+            OpenApiCoverageConsoleRow("GET", "/route2", 200, 2, 80, Remarks.Wip),
+            OpenApiCoverageConsoleRow("GET", "/route2", 400, 2, 80, Remarks.Wip),
+            OpenApiCoverageConsoleRow("POST", "/route2", 201, 1, 80, Remarks.Covered),
+            OpenApiCoverageConsoleRow("POST", "/route2", 400, 6, 80, Remarks.Covered),
+            OpenApiCoverageConsoleRow("POST", "/route2", 503, 0, 80, Remarks.NotCovered),
+            OpenApiCoverageConsoleRow("POST", "/route3", 210, 12, 67, Remarks.Covered),
+            OpenApiCoverageConsoleRow("POST", "/route3", 400, 65, 67, Remarks.Covered),
+            OpenApiCoverageConsoleRow("POST", "/route3", 503, 0, 67, Remarks.NotCovered),
+        )
+
+        val coverageReport = OpenAPICoverageConsoleReport(rows, emptyList(), totalEndpointsCount = 3, missedEndpointsCount = 0, notImplementedAPICount = 0, partiallyMissedEndpointsCount = 0, partiallyNotImplementedAPICount = 0)
+
+        assertThat(coverageReport.totalCoveragePercentage).isEqualTo(81)
     }
 }

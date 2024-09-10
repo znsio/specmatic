238ec30534d4994d3d945bd2550ad99dbf091093
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-09-10 11:18:40 +0530

    Add validate and interactive subcommands for examples command

--- application/src/main/kotlin/application/ExamplesCommand.kt
+++ application/src/main/kotlin/application/ExamplesCommand.kt
@@ -1,73 +1,100 @@
 package application
 
-import io.specmatic.core.*
-import io.specmatic.core.log.*
-import io.specmatic.core.utilities.uniqueNameForApiOperation
-import io.specmatic.core.value.Value
-import io.specmatic.mock.ScenarioStub
+import io.specmatic.core.log.StringLog
+import io.specmatic.core.log.consoleLog
+import io.specmatic.core.log.logger
+import io.specmatic.core.utilities.exitWithMessage
+import io.specmatic.mock.NoMatchingScenario
+import io.specmatic.proxy.ExamplesInteractiveServer
+import io.specmatic.proxy.Proxy
 import picocli.CommandLine.*
-import io.specmatic.test.TestExecutor
 import java.io.File
+import java.lang.Thread.sleep
 import java.util.concurrent.Callable
+import kotlin.system.exitProcess
 
 @Command(name = "examples",
         mixinStandardHelpOptions = true,
-        description = ["Generate externalised JSON example files with API requests and responses"])
+        description = ["Generate externalised JSON example files with API requests and responses"],
+        subcommands = [ExamplesCommand.Validate::class, ExamplesCommand.Interactive::class]
+)
 class ExamplesCommand : Callable<Unit> {
-    @Parameters(index = "0", description = ["Contract file path"])
-    lateinit var contractFile: File
+
+    @Parameters(index = "0", description = ["Contract file path"], arity = "0..1")
+    var contractFile: File? = null
 
     override fun call() {
-        logException {
-            if(!contractFile.exists())
-                throw Exception("Could not find file ${contractFile.path}")
+        if(contractFile == null) {
+            println("No contract file provided. Use a subcommand or provide a contract file. Use --help for more details.")
+            return
+        }
+        try {
+            ExamplesInteractiveServer.generate(contractFile!!)
+        } catch (e: Exception) {
+            exitWithMessage("An unexpected error occurred while generating examples: ${e.message}")
+        }
+    }
 
+    @Command(
+        name = "validate",
+        mixinStandardHelpOptions = true,
+        description = ["Validate the examples"]
+    )
+    class Validate : Callable<Unit> {
+        @Option(names = ["--contract-file"], description = ["Contract file path"], required = true)
+        lateinit var contractFile: File
+
+        @Option(names = ["--example-file"], description = ["Example file path"], required = true)
+        lateinit var exampleFile: File
+
+        override fun call() {
             try {
-                val feature = parseContractFileToFeature(contractFile)
-
-                var ctr = 0
-
-                val examplesDir = contractFile.canonicalFile.parentFile.resolve("""${contractFile.nameWithoutExtension}$EXAMPLES_DIR_SUFFIX""")
-                examplesDir.mkdirs()
-
-                feature.executeTests(object : TestExecutor {
-                    override fun execute(request: HttpRequest): HttpResponse {
-                        ctr += 1
-
-                        val response = feature.lookupResponse(request).cleanup()
-
-                        val scenarioStub = ScenarioStub(request, response)
-
-                        val stubJSON = scenarioStub.toJSON()
-
-                        val stubString = stubJSON.toStringLiteral()
-
-                        val uniqueNameForApiOperation =
-                            uniqueNameForApiOperation(scenarioStub.request, "", scenarioStub.response.status)
-
-                        val filename = "${uniqueNameForApiOperation}_${ctr}.json"
-
-                        val file = examplesDir.resolve(filename)
-                        val loggablePath = "Writing to file: ${file.relativeTo(contractFile.canonicalFile.parentFile).path}"
-
-                        println(loggablePath)
-
-                        file.writeText(stubString)
-
-                        return response
-                    }
-
-                    override fun setServerState(serverState: Map<String, Value>) {
-                    }
-                })
-            } catch(e: StackOverflowError) {
-                logger.log("Got a stack overflow error. You probably have a recursive data structure definition in the contract.")
+                ExamplesInteractiveServer.validate(contractFile, exampleFile)
+                logger.log("The provided example ${exampleFile.name} is valid.")
+            } catch(e: NoMatchingScenario) {
+                logger.log("The provided example ${exampleFile.name} is invalid. Reason:\n")
+                logger.log(e.msg.orEmpty())
+                exitProcess(1)
             }
         }
     }
-}
 
-private fun HttpResponse.cleanup(): HttpResponse {
-    return this.copy(headers = this.headers.minus(SPECMATIC_RESULT_HEADER))
-}
+    @Command(
+        name = "interactive",
+        mixinStandardHelpOptions = true,
+        description = ["Run the example generation interactively"]
+    )
+    class Interactive : Callable<Unit> {
+        @Option(names = ["--contract-file"], description = ["Contract file path"], required = true)
+        lateinit var contractFile: File
 
+        var server: ExamplesInteractiveServer? = null
+
+        override fun call() {
+            server = ExamplesInteractiveServer(
+                serverHost = "localhost",
+                serverPort = 9001,
+                contractFile = contractFile
+            ).also {
+                addShutdownHook()
+            }
+            consoleLog(StringLog("Examples Interactive server is running on http://localhost:9001. Ctrl + C to stop."))
+            while(true) sleep(10000)
+        }
+
+        private fun addShutdownHook() {
+            Runtime.getRuntime().addShutdownHook(object : Thread() {
+                override fun run() {
+                    try {
+                        println("Shutting down examples interactive server...")
+                        server?.close()
+                    } catch (e: InterruptedException) {
+                        currentThread().interrupt()
+                    } catch (e: Throwable) {
+                        logger.log(e)
+                    }
+                }
+            })
+        }
+    }
+}
---
+++ core/src/main/kotlin/io/specmatic/proxy/ExamplesInteractiveServer.kt
@@ -0,0 +1,173 @@
+package io.specmatic.proxy
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.specmatic.core.EXAMPLES_DIR_SUFFIX
+import io.specmatic.core.HttpRequest
+import io.specmatic.core.HttpResponse
+import io.specmatic.core.Result
+import io.specmatic.core.Results
+import io.specmatic.core.SPECMATIC_RESULT_HEADER
+import io.specmatic.core.log.logger
+import io.specmatic.core.parseContractFileToFeature
+import io.specmatic.core.route.modules.HealthCheckModule.Companion.configureHealthCheckModule
+import io.specmatic.core.utilities.uniqueNameForApiOperation
+import io.specmatic.core.value.Value
+import io.specmatic.mock.NoMatchingScenario
+import io.specmatic.mock.ScenarioStub
+import io.specmatic.stub.HttpStub
+import io.specmatic.stub.HttpStubData
+import io.specmatic.test.TestExecutor
+import java.io.Closeable
+import java.io.File
+
+class ExamplesInteractiveServer(
+    private val serverHost: String,
+    private val serverPort: Int,
+    private val contractFile: File
+) : Closeable {
+    private val environment = applicationEngineEnvironment {
+        module {
+            configureHealthCheckModule()
+            routing {
+                post("/_specmatic/examples") {
+                    try {
+                        val generatedExamples: List<String> = generate(contractFile)
+                        call.respond(HttpStatusCode.OK, GenerateExampleResponse(generatedExamples))
+                    } catch(e: Exception) {
+                        call.respond(HttpStatusCode.InternalServerError, "An unexpected error occurred: ${e.message}")
+                    }
+                }
+
+                post("/_specmatic/examples/validate") {
+                    val request = call.receive<ValidateExampleRequest>()
+                    try {
+                        validate(
+                            contractFile,
+                            File(request.exampleFile)
+                        )
+                        call.respond(HttpStatusCode.OK, "The provided example is valid.")
+                    } catch(e: NoMatchingScenario) {
+                        call.respond(HttpStatusCode.BadRequest, mapOf("error" to e.msg))
+                    } catch(e: Exception) {
+                        call.respond(HttpStatusCode.InternalServerError, "An unexpected error occurred: ${e.message}")
+                    }
+                }
+            }
+        }
+        connector {
+            this.host = serverHost
+            this.port = serverPort
+        }
+    }
+
+    private val server: ApplicationEngine = embeddedServer(Netty, environment, configure = {
+        this.requestQueueLimit = 1000
+        this.callGroupSize = 5
+        this.connectionGroupSize = 20
+        this.workerGroupSize = 20
+    })
+
+    init {
+        server.start()
+    }
+
+    override fun close() {
+        server.stop(0, 0)
+    }
+
+    companion object {
+        fun generate(contractFile: File): List<String> {
+            val generatedExampleFiles = mutableListOf<String>()
+                if(!contractFile.exists())
+                    throw Exception("Could not find file ${contractFile.path}")
+
+            try {
+                val feature = parseContractFileToFeature(contractFile)
+
+                var ctr = 0
+
+                val examplesDir =
+                    contractFile.canonicalFile.parentFile.resolve("""${contractFile.nameWithoutExtension}$EXAMPLES_DIR_SUFFIX""")
+                examplesDir.mkdirs()
+
+                feature.executeTests(object : TestExecutor {
+                    override fun execute(request: HttpRequest): HttpResponse {
+                        ctr += 1
+
+                        val response = feature.lookupResponse(request).cleanup()
+
+                        val scenarioStub = ScenarioStub(request, response)
+
+                        val stubJSON = scenarioStub.toJSON()
+
+                        val stubString = stubJSON.toStringLiteral()
+
+                        val uniqueNameForApiOperation =
+                            uniqueNameForApiOperation(scenarioStub.request, "", scenarioStub.response.status)
+
+                        val filename = "${uniqueNameForApiOperation}_${ctr}.json"
+
+                        val file = examplesDir.resolve(filename)
+                        val loggablePath =
+                            "Writing to file: ${file.relativeTo(contractFile.canonicalFile.parentFile).path}"
+
+                        println(loggablePath)
+
+                        file.writeText(stubString)
+                        generatedExampleFiles.add(file.absolutePath)
+
+                        return response
+                    }
+
+                    override fun setServerState(serverState: Map<String, Value>) {
+                    }
+                })
+            } catch (e: StackOverflowError) {
+                logger.log("Got a stack overflow error. You probably have a recursive data structure definition in the contract.")
+                throw e
+            }
+            return generatedExampleFiles
+        }
+
+        fun validate(contractFile: File, exampleFile: File): List<HttpStubData> {
+            val scenarioStub = ScenarioStub.readFromFile(exampleFile)
+            val feature = parseContractFileToFeature(contractFile)
+
+            val result: Pair<Pair<Result.Success, List<HttpStubData>>?, NoMatchingScenario?> =
+                HttpStub.setExpectation(scenarioStub, feature)
+            val validationResult = result.first
+            val noMatchingScenario = result.second
+
+            if(validationResult == null) {
+                val failures =  noMatchingScenario?.results?.withoutFluff()?.results ?: emptyList()
+
+                val failureResults = Results(failures).withoutFluff()
+                throw NoMatchingScenario(
+                    failureResults,
+                    cachedMessage = failureResults.report(scenarioStub.request),
+                    msg = failureResults.report(scenarioStub.request)
+                )
+            }
+
+            return validationResult.second
+        }
+
+        private fun HttpResponse.cleanup(): HttpResponse {
+            return this.copy(headers = this.headers.minus(SPECMATIC_RESULT_HEADER))
+        }
+    }
+}
+
+data class ValidateExampleRequest(
+    val exampleFile: String
+)
+
+data class GenerateExampleResponse(
+    val generatedExamples: List<String>
+)

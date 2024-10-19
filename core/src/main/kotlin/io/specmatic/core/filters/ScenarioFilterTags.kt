0687fe1a185912f9adaf4147a0f5daa9dfd97acb
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-10-19 14:07:39 +0530

    Use existing breadcrumbs as filter tags and write tests for ScenarioMetadataFilter

--- core/src/main/kotlin/io/specmatic/core/filters/ScenarioFilterTags.kt
+++ core/src/main/kotlin/io/specmatic/core/filters/ScenarioFilterTags.kt
@@ -1,10 +1,15 @@
 package io.specmatic.core.filters
 
+import io.specmatic.core.HEADERS_BREADCRUMB
+import io.specmatic.core.METHOD_BREAD_CRUMB
+import io.specmatic.core.PATH_BREAD_CRUMB
+import io.specmatic.core.QUERY_PARAMS_BREADCRUMB
+
 enum class ScenarioFilterTags(val key: String) {
-    METHOD("method"),
-    PATH("path"),
-    STATUS_CODE("status-code"),
-    HEADER("header"),
-    QUERY("query"),
-    EXAMPLE_NAME("example-name")
+    METHOD(METHOD_BREAD_CRUMB),
+    PATH(PATH_BREAD_CRUMB),
+    STATUS_CODE("STATUS-CODE"),
+    HEADER(HEADERS_BREADCRUMB),
+    QUERY(QUERY_PARAMS_BREADCRUMB),
+    EXAMPLE_NAME("EXAMPLE-NAME")
 }

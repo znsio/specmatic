cbdaebb968083f136cc17f4759f4a036ee667a5b
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-10-21 15:14:52 +0530

    Change the STATUS-CODE tag to STATUS in the filter tags

--- core/src/main/kotlin/io/specmatic/core/filters/ScenarioFilterTags.kt
+++ core/src/main/kotlin/io/specmatic/core/filters/ScenarioFilterTags.kt
@@ -4,11 +4,12 @@ import io.specmatic.core.HEADERS_BREADCRUMB
 import io.specmatic.core.METHOD_BREAD_CRUMB
 import io.specmatic.core.PATH_BREAD_CRUMB
 import io.specmatic.core.QUERY_PARAMS_BREADCRUMB
+import io.specmatic.core.STATUS_BREAD_CRUMB
 
 enum class ScenarioFilterTags(val key: String) {
     METHOD(METHOD_BREAD_CRUMB),
     PATH(PATH_BREAD_CRUMB),
-    STATUS_CODE("STATUS-CODE"),
+    STATUS_CODE(STATUS_BREAD_CRUMB),
     HEADER(HEADERS_BREADCRUMB),
     QUERY(QUERY_PARAMS_BREADCRUMB),
     EXAMPLE_NAME("EXAMPLE-NAME")

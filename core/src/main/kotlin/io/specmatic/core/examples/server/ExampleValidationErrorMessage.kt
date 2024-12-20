968fdd9539a4ffc864b8f7ca4cea07979e0276a9
Samy <samy@Samys-MacBook-Air.local>
2024-12-20 10:09:13 +0530

    Comments on challenge faced

--- core/src/main/kotlin/io/specmatic/core/examples/server/ExampleValidationErrorMessage.kt
+++ core/src/main/kotlin/io/specmatic/core/examples/server/ExampleValidationErrorMessage.kt
@@ -18,6 +18,26 @@ private const val HTTP_QUERY_PARAMS = "query"
 
 data class ExampleValidationErrorMessage(val failureDetails: List<MatchFailureDetails>) {
     fun jsonPathToErrorDescriptionMapping(): List<Map<String, Any>> {
+        //On Example Load
+        //Error Message Ex- Key @type in the specification is missing from the example
+        //bread crumb = ArrayList - 1 - REQUEST, 2-BODY, 3-@type
+
+        /* On click of validate
+            Error Message-
+            In scenario "Create a subscription (hub) to receive Events. Response: Notified"
+            API: POST /hub -> 201
+
+                >> REQUEST.BODY.@type
+
+                Key @type in the specification is missing from the example
+
+
+                >> RESPONSE.BODY.@types
+
+                Key @types in the example is not in the specification
+
+              BreadCrumbs - Empty Array List
+         */
         return failureDetails.flatMap { failureDetail ->
             val transformedJsonPaths = failureDetail.transformBreadcrumbs()
             listOf(

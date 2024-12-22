0072061e0db2a18c0c5477535be9e5d71ba17dc7
Samy <samy@Samys-MacBook-Air.local>
2024-12-22 15:59:51 +0530

    Amber Validation BE

--- core/src/main/kotlin/io/specmatic/core/examples/server/ExampleValidationErrorMessage.kt
+++ core/src/main/kotlin/io/specmatic/core/examples/server/ExampleValidationErrorMessage.kt
@@ -1,35 +1,70 @@
 package io.specmatic.core.examples.server
 
+import io.specmatic.core.MatchFailureDetails
+
 private const val JSON_PATH = "jsonPath"
 private const val DESCRIPTION = "description"
-private const val BREADCRUMB_PREFIX = ">>"
-private const val BREADCRUMB_PREFIX_WITH_TRAILING_SPACE = "$BREADCRUMB_PREFIX "
+private const val SEVERITY = "severity"
 private const val BREADCRUMB_DELIMITER = "."
 private const val JSONPATH_DELIMITER = "/"
 private const val HTTP_RESPONSE = "http-response"
 private const val HTTP_REQUEST = "http-request"
+private const val BREADCRUMB_PREFIX = ">>"
+
 private const val BREAD_CRUMB_HEADERS = "HEADERS"
 private const val HTTP_HEADERS = "headers"
 const val BREADCRUMB_QUERY_PARAMS = "QUERY-PARAMS"
 private const val HTTP_QUERY_PARAMS = "query"
 
-data class ExampleValidationErrorMessage(val fullErrorMessageString: String) {
-    fun jsonPathToErrorDescriptionMapping(): List<Map<String, String>> {
-        val jsonPaths = jsonPathsForAllErrors(fullErrorMessageString)
-        val descriptions = extractDescriptions(fullErrorMessageString)
-        val map: List<Map<String, String>> = jsonPaths.zip(descriptions) { jsonPath, description ->
-            mapOf(JSON_PATH to jsonPath, DESCRIPTION to description)
-        }
-        return map
-    }
+data class ExampleValidationErrorMessage(val failureDetails: List<MatchFailureDetails>) {
+    fun jsonPathToErrorDescriptionMapping(): List<Map<String, Any>> {
+        //On Example Load
+        //Error Message Ex- Key @type in the specification is missing from the example
+        //bread crumb = ArrayList - 1 - REQUEST, 2-BODY, 3-@type
 
-    private fun jsonPathsForAllErrors(errorMessage: String): List<String> {
-        val breadcrumbs = errorMessage.lines().map { it.trim() }.filter { it.startsWith(BREADCRUMB_PREFIX_WITH_TRAILING_SPACE) }.map {
-            it.removePrefix(
-                BREADCRUMB_PREFIX_WITH_TRAILING_SPACE
+        /* On click of validate
+            Error Message-
+            In scenario "Create a subscription (hub) to receive Events. Response: Notified"
+            API: POST /hub -> 201
+
+                >> REQUEST.BODY.@type
+
+                Key @type in the specification is missing from the example
+
+
+                >> RESPONSE.BODY.@types
+
+                Key @types in the example is not in the specification
+
+              BreadCrumbs - Empty Array List
+         */
+        return failureDetails.flatMap { failureDetail ->
+            val transformedJsonPaths = failureDetail.transformBreadcrumbs()
+            listOf(
+                mapOf(
+                    JSON_PATH to transformedJsonPaths,
+                    DESCRIPTION to failureDetail.errorMessages.joinToString(", "),
+                    SEVERITY to if (failureDetail.isPartial) "warning" else "error"
+                )
             )
         }
-        return breadcrumbs.map { breadcrumb ->
+    }
+
+
+    private fun MatchFailureDetails.transformBreadcrumbs(): List<String> {
+        val breadCrumbs = if (this.breadCrumbs.isEmpty()) {
+            this.errorMessages
+                .flatMap { message ->
+                    message.lines()
+                }
+                .filter { it.trim().startsWith(BREADCRUMB_PREFIX) }
+                .map {
+                    it.removePrefix(BREADCRUMB_PREFIX).trim()
+                }
+        } else {
+            listOf(this.breadCrumbs.joinToString("."))
+        }
+        return breadCrumbs.map { breadcrumb ->
             breadcrumb
                 .replace("RESPONSE", HTTP_RESPONSE)
                 .replace("REQUEST", HTTP_REQUEST)
@@ -40,13 +75,5 @@ data class ExampleValidationErrorMessage(val fullErrorMessageString: String) {
                 .replace(Regex("\\[(\\d+)]")) { matchResult -> "/${matchResult.groupValues[1]}" }
                 .let { "/${it.trimStart('/')}" }
         }
-    }
-
-    private fun extractDescriptions(reportString: String): List<String> {
-        val parts = reportString.split(BREADCRUMB_PREFIX)
-
-        return parts.drop(1)
-            .map { "$BREADCRUMB_PREFIX$it".trim() }
-            .filter { it.isNotBlank() }
     }
 }

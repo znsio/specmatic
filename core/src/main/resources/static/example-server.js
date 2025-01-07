5e0a899661aec0fb0d21b0ac63e689016041122b
Samy <samy@Samys-MacBook-Air.local>
2025-01-07 12:06:11 +0530

    UI Wiring for Amber Validation of Specmatic Warnings

--- core/src/main/resources/static/example-server.js
+++ core/src/main/resources/static/example-server.js
@@ -745,7 +745,7 @@ function highlightErrorLines(editor, metadata, exampleJson) {
             }
             existingMarkers.get(lineNumber).push(meta.description);
             const combinedDescriptions = existingMarkers.get(lineNumber).join('\n\n');
-            const className = "specmatic-editor-line-error";
+            const className = meta.isPartial ? "specmatic-editor-line-warning": "specmatic-editor-line-error";
             const tokenStart = lineLength.from;
             const tokenEnd = lineLength.to;
             const existingDecoration = decorations.filter(decoration => decoration.from === tokenStart && decoration.to === tokenEnd);
@@ -815,6 +815,7 @@ const createErrorTooltipExtension = (errorMetadata) => {
                     tooltip.textContent = error.message;
                     tooltip.innerHTML = error.message.replace(/\n/g, "<br>");
                     tooltip.className = "specmatic-editor-tooltip";
+                    tooltip.style.borderLeft = error.isPartial ? "4px solid yellow" : "4px solid red";
                     tooltip.style.top = `${lineCoords.top + window.scrollY - tooltip.offsetHeight - numberOfPixelsAboveDefaultOffset}px`;
                     tooltip.style.left = `${lineCoords.left + numberOfPixelsLeftOfDefaultToolTipOrigin}px`;
                     return {dom: tooltip};

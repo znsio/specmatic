43ebed55da2ed01b6c5831fb7fb21c7e21002b80
Sukesh Seth <sukeshseth20@gmail.com>
2025-01-10 14:57:09 +0530

    Update version 2 of Specmatic Config, move config mapper to package and add version 1 for specmatic config

--- core/src/main/kotlin/io/specmatic/core/config/v2/ContractConfig.kt
+++ core/src/main/kotlin/io/specmatic/core/config/v2/ContractConfig.kt
@@ -1,24 +1,39 @@
 package io.specmatic.core.config.v2
 
-import io.specmatic.core.config.v1.Source
-import io.specmatic.core.config.v1.SourceProvider
+import io.specmatic.core.Source
+import io.specmatic.core.SourceProvider
 
 data class ContractConfig(
-    val type: SourceProvider = SourceProvider.filesystem,
-    val url: String? = null,
-    val branch: String? = null,
-    val directory: String? = null,
+    val git: GitConfig? = null,
+    val filesystem: FileSystemConfig? = null,
     val provides: List<String>? = null,
     val consumes: List<String>? = null
 ) {
     fun transform(): Source {
-        return Source(
-            provider = this.type,
-            repository = this.url,
-            branch = this.branch,
-            test = this.provides,
-            stub = this.consumes,
-            directory = this.directory
-        )
+        return when {
+            git != null -> Source(
+                provider = SourceProvider.git,
+                repository = git.url,
+                branch = git.branch,
+                test = provides,
+                stub = consumes
+            )
+
+            else -> Source(
+                directory = filesystem?.directory,
+                provider = SourceProvider.filesystem,
+                test = provides,
+                stub = consumes,
+            )
+        }
     }
 }
+
+data class GitConfig(
+    val url: String? = null,
+    val branch: String? = null
+)
+
+data class FileSystemConfig(
+    val directory: String? = null
+)

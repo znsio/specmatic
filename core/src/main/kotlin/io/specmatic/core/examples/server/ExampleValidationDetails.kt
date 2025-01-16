f3f57212364ab165691e70ecc1f943e627c4529f
Sufiyan <StarKhan6368@gmail.com>
2025-01-16 15:28:14 +0530

    Refactor ExampleValidationDetails
     Add tests for tilde breadcrumbs.

--- core/src/main/kotlin/io/specmatic/core/examples/server/ExampleValidationDetails.kt
+++ core/src/main/kotlin/io/specmatic/core/examples/server/ExampleValidationDetails.kt
@@ -12,78 +12,63 @@ private const val HTTP_REQUEST = "http-request"
 private const val BREAD_CRUMB_HEADERS = "HEADERS"
 private const val HTTP_HEADERS = "headers"
 private const val HTTP_BODY = "body"
-const val BREADCRUMB_QUERY_PARAMS = "QUERY-PARAMS"
+private const val BREADCRUMB_QUERY_PARAMS = "QUERY-PARAMS"
 private const val HTTP_QUERY_PARAMS = "query"
-private const val BREADCRUMB_TILDLE = ".(~~~"
-private const val BREADCRUMB_REGEX_TILDLE = "^\\(~~~"
-private const val BREADCRUMB_WHEN_OBJ = "(when "
 
 data class ExampleValidationDetails(val matchFailureDetailsList: List<MatchFailureDetails>) {
+
     fun jsonPathToErrorDescriptionMapping(): List<ExampleValidationResult> {
-        val sortedMatchFailureDetailsList = matchFailureDetailsList.sortedBy { it.isPartial }
+        if (matchFailureDetailsList.isEmpty()) return emptyList()
 
-        val jsonPaths = jsonPathsForAllErrors(sortedMatchFailureDetailsList)
-        val descriptions = extractDescriptions(sortedMatchFailureDetailsList)
-        val severities = extractSeverities(sortedMatchFailureDetailsList)
-        val maxSize = listOf(jsonPaths.size, descriptions.size, severities.size).minOrNull() ?: 0
-        return jsonPaths.take(maxSize).zip(descriptions.take(maxSize))
-            .zip(severities.take(maxSize)) { (jsonPath, description), severity ->
-                ExampleValidationResult(
-                    jsonPath = jsonPath,
-                    description = description,
-                    severity = severity
-                )
-            }
-    }
-
-    private fun jsonPathsForAllErrors(matchingFailureDetails: List<MatchFailureDetails>): List<String> {
-        return matchingFailureDetails.flatMap { matchingFailureDetail ->
-            val combinedBreadcrumbs = matchingFailureDetail.breadCrumbs
-                .takeIf { it.isNotEmpty() }
-                ?.filterNot { it.startsWith("(") && it.endsWith(")") }
-                ?.joinToString(JSONPATH_DELIMITER) { breadcrumb -> processBreadcrumb(breadcrumb) }
-                ?.let { "/$it" }
-                ?: ""
-            listOf(combinedBreadcrumbs)
+        return matchFailureDetailsList.sortedBy { it.isPartial }.map {
+            val jsonPaths = processBreadcrumbs(it.breadCrumbs)
+            ExampleValidationResult(
+                jsonPath = jsonPaths.withoutTildeBreadCrumb().joinToString(JSONPATH_DELIMITER, prefix = JSONPATH_DELIMITER),
+                description = processDescription(it.errorMessages, jsonPaths.jsonPathForDescription()),
+                severity = extractSeverity(it)
+            )
         }
     }
 
-    private fun processBreadcrumb(breadcrumb: String): String {
-        return breadcrumb
-            .replace(BREADCRUMB_RESPONSE, HTTP_RESPONSE)
+    private fun processBreadcrumbs(breadcrumbs: List<String>): List<String> {
+        return breadcrumbs.filter { it.isNotBlank() }.map {
+            it.replace(BREADCRUMB_RESPONSE, HTTP_RESPONSE)
             .replace(BREADCRUMB_REQUEST, HTTP_REQUEST)
             .replace(BREADCRUMB_BODY, HTTP_BODY)
             .replace(BREAD_CRUMB_HEADERS, HTTP_HEADERS)
             .replace(BREADCRUMB_QUERY_PARAMS, HTTP_QUERY_PARAMS)
             .replace(BREADCRUMB_DELIMITER, JSONPATH_DELIMITER)
-            .replace(BREADCRUMB_TILDLE, " $BREADCRUMB_WHEN_OBJ")
-            .replace(Regex(BREADCRUMB_REGEX_TILDLE), BREADCRUMB_WHEN_OBJ)
-            .replace(Regex("\\[(\\d+)]")) { matchResult -> "/${matchResult.groupValues[1]}" }.trimStart('/')
-    }
-
-    private fun extractDescriptions(matchingFailureDetails: List<MatchFailureDetails>): List<String> {
-        return matchingFailureDetails.flatMap { matchingFailureDetail ->
-            matchingFailureDetail.errorMessages.map { errorMessage ->
-                val breadcrumb = matchingFailureDetail.breadCrumbs
-                    .joinToString(BREADCRUMB_DELIMITER) { breadcrumbPart ->
-                        if (breadcrumbPart.matches(Regex("\\[(\\d+)]"))) {
-                            "[${processBreadcrumb(breadcrumbPart)}]"
-                        } else {
-                            processBreadcrumb(breadcrumbPart)
-                        }
-                    }
-                ">> $breadcrumb\n\n$errorMessage"
-            }
+            .replace(Regex("\\[(\\d+)]")) { matchResult -> matchResult.groupValues[1] }
         }
     }
 
-    private fun extractSeverities(matchFailureDetailsList: List<MatchFailureDetails>): List<Severity> {
-        return matchFailureDetailsList.map { matchFailureDetails ->
-            if (matchFailureDetails.isPartial) {
-                Severity.WARNING
-            } else {
-                Severity.ERROR
+    private fun processDescription(errorMessages: List<String>, breadCrumbs: String): String {
+        val errorMessage = errorMessages.joinToString("\n")
+        if (breadCrumbs.isEmpty()) return errorMessage
+        return ">> ${breadCrumbs}\n\n$errorMessage"
+    }
+
+    private fun extractSeverity(matchFailureDetails: MatchFailureDetails): Severity {
+        return if (matchFailureDetails.isPartial) {
+            Severity.WARNING
+        } else Severity.ERROR
+    }
+
+    private fun List<String>.withoutTildeBreadCrumb(): List<String> {
+        return this.filterNot { it.isTildeBreadCrumb() }
+    }
+
+    private fun List<String>.jsonPathForDescription(): String {
+        return this.joinToString("") {
+            when {
+                it.isTildeBreadCrumb() -> it.removePrefix(JSONPATH_DELIMITER).replace(Regex("\\.?\\(~{3}"), " (when")
+                it.all(Char::isDigit) -> ".[$it]"
+                else -> ".$it"
             }
-        }
+        }.removePrefix(".")
+    }
+
+    private fun String.isTildeBreadCrumb(): Boolean  {
+        return this.removePrefix(JSONPATH_DELIMITER).startsWith("(") && this.endsWith(")")
     }
 }

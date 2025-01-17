b8d2dca54af4ea67e7b265169a4366e6e76d05a4
Sukesh Seth <sukeshseth20@gmail.com>
2025-01-17 23:04:43 +0530

    Add tests for Contract Config serializer & deserializer

--- core/src/test/kotlin/io/specmatic/core/config/SpecmaticConfigAllTest.kt
+++ core/src/test/kotlin/io/specmatic/core/config/SpecmaticConfigAllTest.kt
@@ -1,8 +1,14 @@
 package io.specmatic.core.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.specmatic.core.Source
 import io.specmatic.core.SourceProvider
 import io.specmatic.core.SpecmaticConfig
+import io.specmatic.core.config.v2.ContractConfig
+import io.specmatic.core.config.v2.ContractSource
+import io.specmatic.core.config.v2.SpecmaticConfigV2
 import io.specmatic.core.loadSpecmaticConfig
 import io.specmatic.core.pattern.ContractException
 import org.assertj.core.api.Assertions.assertThat
@@ -10,6 +16,8 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.skyscreamer.jsonassert.JSONAssert
+import org.skyscreamer.jsonassert.JSONCompareMode
 import java.io.File
 
 internal class SpecmaticConfigAllTest {
@@ -74,5 +82,70 @@ internal class SpecmaticConfigAllTest {
         val specmaticConfigWithInvalidVersion = File(configFile)
         val exception = assertThrows<ContractException> { specmaticConfigWithInvalidVersion.toSpecmaticConfig() }
         assertThat(exception.message).isEqualTo("Unsupported Specmatic config version")
+    }
+
+    @CsvSource(
+        "./src/test/resources/specmaticConfigFiles/specmatic_config_v2.yaml",
+        "./src/test/resources/specmaticConfigFiles/specmatic_config_v2.json"
+    )
+    @ParameterizedTest
+    fun `should serialize ContractConfig successfully`(configFile: String) {
+        val objectMapper = ObjectMapper(YAMLFactory()).registerKotlinModule()
+        val specmaticConfigV2 = objectMapper.readValue(File(configFile).readText(), SpecmaticConfigV2::class.java)
+
+        val contracts = specmaticConfigV2.contracts
+
+        assertThat(contracts.size).isEqualTo(2)
+        assertThat(contracts[0].contractSource).isInstanceOf(ContractSource.Git::class.java)
+        val gitContractSource = contracts[0].contractSource as ContractSource.Git
+        assertThat(gitContractSource.url).isEqualTo("https://contracts")
+        assertThat(gitContractSource.branch).isEqualTo("1.0.1")
+        assertThat(contracts[0].provides).containsOnly("com/petstore/1.yaml")
+        assertThat(contracts[0].consumes).containsOnly("com/petstore/payment.yaml")
+
+        assertThat(contracts[1].contractSource).isInstanceOf(ContractSource.FileSystem::class.java)
+        val fileSystemContractSource = contracts[1].contractSource as ContractSource.FileSystem
+        assertThat(fileSystemContractSource.directory).isEqualTo("contracts")
+        assertThat(contracts[1].provides).containsOnly("com/petstore/1.yaml")
+        assertThat(contracts[1].consumes).containsOnly("com/petstore/payment.yaml", "com/petstore/order.yaml")
+    }
+
+    @Test
+    fun `should deserialize ContractConfig successfully`() {
+        val expectedContractsJson = """[
+            {
+                "git": {
+                    "url": "https://contracts",
+                    "branch": "1.0.1"
+                },
+                "provides": ["com/petstore/1.yaml"],
+                "consumes": ["com/petstore/payment.yaml"]
+            },
+            {
+                "filesystem": {
+                    "directory": "contracts"
+                },
+                "provides": ["com/petstore/1.yaml"],
+                "consumes": ["com/petstore/payment.yaml", "com/petstore/order.yaml"]
+            }
+        ]"""
+
+        val contracts = listOf(
+            ContractConfig(
+                contractSource = ContractSource.Git(url = "https://contracts", branch = "1.0.1"),
+                provides = listOf("com/petstore/1.yaml"),
+                consumes = listOf("com/petstore/payment.yaml")
+            ),
+            ContractConfig(
+                contractSource = ContractSource.FileSystem(directory = "contracts"),
+                provides = listOf("com/petstore/1.yaml"),
+                consumes = listOf("com/petstore/payment.yaml", "com/petstore/order.yaml")
+            )
+        )
+
+        val objectMapper = ObjectMapper().registerKotlinModule()
+        val contractsJson = objectMapper.writeValueAsString(contracts)
+
+        JSONAssert.assertEquals(expectedContractsJson, contractsJson, JSONCompareMode.STRICT)
     }
 }

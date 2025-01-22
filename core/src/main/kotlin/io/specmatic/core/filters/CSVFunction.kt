b868f5674d6eef54c64abe4903efca9102a1ad9b
Samyak Gantayat <samyakgantayat123@gmail.com>
2025-01-22 22:12:14 +0530

    CSV Function Code Optimisation

--- core/src/main/kotlin/io/specmatic/core/filters/CSVFunction.kt
+++ core/src/main/kotlin/io/specmatic/core/filters/CSVFunction.kt
@@ -14,19 +14,40 @@ class CSVFunction : AbstractFunction() {
         expression: Expression, functionToken: Token, vararg parameterValues: EvaluationValue
     ): EvaluationValue {
         val inputString = parameterValues[0].stringValue
-
         val (label, operator, values) = parseCondition(inputString)
-        val scenarioValue = expression.dataAccessor.getData(label).value
-        val result = when (operator) {
-            "=" -> values.any { it == scenarioValue.toString() || (label == ScenarioFilterTags.STATUS_CODE.key && isInRange(it, scenarioValue.toString()))
-                    || (label == ScenarioFilterTags.PATH.key && it.contains("*") && Pattern.compile(it.replace("*", ".*")).matcher(scenarioValue.toString()).matches())}
-            "!=" -> values.all { it != scenarioValue.toString() && (label != ScenarioFilterTags.STATUS_CODE.key || !isInRange(it, scenarioValue.toString())) &&
-                    (label == ScenarioFilterTags.PATH.key && !it.contains("*") || !Pattern.compile(it.replace("*", ".*")).matcher(scenarioValue.toString()).matches())}
-            else -> throw IllegalArgumentException("Unsupported operator: $operator")
-        }
+        val scenarioValue = expression.dataAccessor.getData(label).value.toString()
+        val result = evaluateCondition(label, operator, values, scenarioValue)
 
         return EvaluationValue.of(result, ExpressionConfiguration.defaultConfiguration())
     }
+
+    private fun evaluateCondition(label: String, operator: String, values: List<String>, scenarioValue: String): Boolean {
+
+        fun checkCondition(value: String, isMatch: Boolean): Boolean {
+            val matches = when (label) {
+                ScenarioFilterTags.STATUS_CODE.key -> value == scenarioValue || matchesStatusCode(value, scenarioValue)
+                ScenarioFilterTags.PATH.key -> value == scenarioValue || matchesPath(value, scenarioValue)
+                else -> value == scenarioValue
+            }
+            return if (isMatch) matches else !matches
+        }
+
+        return when (operator) {
+            "=" -> values.any { checkCondition(it, isMatch = true) }
+            "!=" -> values.all { checkCondition(it, isMatch = false) }
+            else -> throw IllegalArgumentException("Unsupported operator: $operator")
+        }
+    }
+
+
+    private fun matchesStatusCode(value: String, scenarioValue: String): Boolean {
+        return isInRange(value, scenarioValue)
+    }
+
+    private fun matchesPath(value: String, scenarioValue: String): Boolean {
+        return value.contains("*") && Pattern.compile(value.replace("*", ".*")).matcher(scenarioValue.toString()).matches()
+    }
+
 
     private fun parseCondition(condition: String): Triple<String, String, List<String>> {
         val operator = if (condition.contains("!=")) "!=" else "="
@@ -40,19 +61,26 @@ class CSVFunction : AbstractFunction() {
     }
 
     private fun isInRange(range: String, value: String): Boolean {
-        val intValue = value.toIntOrNull() ?: return false
+        val metadataValue = value.toIntOrNull() ?: return false
+
         return when {
             range.endsWith("xx") -> {
-                val rangeStart = range.dropLast(2).toInt() * 100
-                val rangeEnd = rangeStart + 99
-                intValue in rangeStart..rangeEnd
+                isWithinBounds(range,100, metadataValue)
             }
             range.endsWith("x") -> {
-                val rangeStart = range.dropLast(1).toInt() * 10
-                val rangeEnd = rangeStart + 9
-                intValue in rangeStart..rangeEnd
+               isWithinBounds(range, 10, metadataValue)
             }
             else -> false
         }
+    }
+
+    private fun isWithinBounds(range: String, multiplier: Int, value: Int): Boolean {
+        val bounds = getRangeBounds(range, multiplier)
+        return bounds?.contains(value) ?: false
+    }
+
+    private fun getRangeBounds(range: String, multiplier: Int): IntRange? {
+        val rangeStart = range.dropLast(multiplier.toString().length-1).toIntOrNull()?.times(multiplier)
+        return rangeStart?.let { it..<it + (multiplier - 1) }
     }
 }

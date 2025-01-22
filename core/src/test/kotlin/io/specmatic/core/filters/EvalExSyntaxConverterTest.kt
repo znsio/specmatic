aa74958a71df5af054f6e668b096976e72b9787a
Samyak Gantayat <samyakgantayat123@gmail.com>
2025-01-22 17:13:43 +0530

    test case names standardized

--- core/src/test/kotlin/io/specmatic/core/filters/EvalExSyntaxConverterTest.kt
+++ core/src/test/kotlin/io/specmatic/core/filters/EvalExSyntaxConverterTest.kt
@@ -6,77 +6,77 @@ import org.junit.jupiter.api.Test
 
 class EvalExSyntaxConverterTest {
 
-    val evalExSyntaxConverter = EvalExSyntaxConverter()
+    private val evalExSyntaxConverter = EvalExSyntaxConverter()
 
     @Test
-    fun `test standardizeExpression with only METHOD expression`() {
+    fun `test standard expression with only METHOD expression`() {
         val expression = "METHOD=GET"
         val expected = "METHOD=\"GET\""
-        val standardExpressions= evalExSyntaxConverter.standardizeExpression(expression)
-        assertEquals(expected,standardExpressions)
+        val standardExpression = evalExSyntaxConverter.standardizeExpression(expression)
+        assertEquals(expected,standardExpression)
     }
 
     @Test
-    fun `test standardizeExpression with METHOD and STATUS expression`() {
+    fun `test standard expression with METHOD and STATUS expression`() {
         val expression = "METHOD=GET && STATUS=200,400"
         val expected = "METHOD=\"GET\" && CSV(\"STATUS=200,400\")"
-        val standardExpressions= evalExSyntaxConverter.standardizeExpression(expression)
-        assertEquals(expected,standardExpressions)
+        val standardExpression= evalExSyntaxConverter.standardizeExpression(expression)
+        assertEquals(expected,standardExpression)
     }
 
     @Test
-    fun `test standardizeExpression with multiple METHOD and STATUS expressions`() {
+    fun `test standard expression with multiple METHOD and STATUS expression`() {
         val expression = "METHOD=GET,POST && STATUS=200,400"
         val expected = "CSV(\"METHOD=GET,POST\") && CSV(\"STATUS=200,400\")"
-        val standardExpressions= evalExSyntaxConverter.standardizeExpression(expression)
-        assertEquals(expected,standardExpressions)
+        val standardExpression= evalExSyntaxConverter.standardizeExpression(expression)
+        assertEquals(expected,standardExpression)
     }
 
     @Test
-    fun `test standardizeExpression with multiple METHOD and PATH expressions`() {
+    fun `test standard expression with multiple METHOD and PATH expression`() {
         val expression = "METHOD=GET,POST || PATH=/users,/user(id:string)"
         val expected = "CSV(\"METHOD=GET,POST\") || CSV(\"PATH=/users,/user(id:string)\")"
-        val standardExpressions= evalExSyntaxConverter.standardizeExpression(expression)
-        assertEquals(expected,standardExpressions)
+        val standardExpression= evalExSyntaxConverter.standardizeExpression(expression)
+        assertEquals(expected,standardExpression)
     }
 
     @Test
-    fun `test standardizeExpression with multiple METHOD and single PATH expressions`() {
+    fun `test standard expression with multiple METHOD and single PATH expression`() {
         val expression = "(METHOD=POST && PATH=/users) || (METHOD=POST && PATH=/products)"
         val expected = "( METHOD=\"POST\" && PATH=\"/users\" ) || ( METHOD=\"POST\" && PATH=\"/products\" )"
-        val standardExpressions= evalExSyntaxConverter.standardizeExpression(expression)
-        assertEquals(expected,standardExpressions)
+        val standardExpression= evalExSyntaxConverter.standardizeExpression(expression)
+        assertEquals(expected,standardExpression)
     }
 
     @Test
-    fun `test standardizeExpression with STATUS expression`() {
+    fun `test standard expression with STATUS expression`() {
         val expression = "STATUS=2xx"
         val expected = "CSV(\"STATUS=2xx\")"
-        val standardExpressions= evalExSyntaxConverter.standardizeExpression(expression)
-        assertEquals(expected,standardExpressions)
+        val standardExpression= evalExSyntaxConverter.standardizeExpression(expression)
+        assertEquals(expected,standardExpression)
     }
 
     @Test
-    fun `test standardizeExpression with PATH expression`() {
+    fun `test standard expression with PATH expression`() {
         val expression = "STATUS!=202 && PATH!=/hub,/hub/(id:string)"
         val expected = "STATUS!=202 && CSV(\"PATH!=/hub,/hub/(id:string)\")"
-        val standardExpressions= evalExSyntaxConverter.standardizeExpression(expression)
-        assertEquals(expected,standardExpressions)
+        val standardExpression= evalExSyntaxConverter.standardizeExpression(expression)
+        assertEquals(expected,standardExpression)
     }
 
     @Test
-    fun `test standardizeExpression with QUERY expression`() {
+    fun `test standard expression with QUERY expression`() {
         val expression = "QUERY=fields"
         val expected = "QUERY=\"fields\""
-        val standardExpressions= evalExSyntaxConverter.standardizeExpression(expression)
-        assertEquals(expected,standardExpressions)
+        val standardExpression= evalExSyntaxConverter.standardizeExpression(expression)
+        assertEquals(expected,standardExpression)
     }
 
     @Test
-    fun `test standardizeExpression with empty expression`() {
+    fun `test standard expression with empty expression`() {
         val expression = ""
         val expected = ""
-        val standardExpressions= evalExSyntaxConverter.standardizeExpression(expression)
-        assertEquals(expected,standardExpressions)
+        val standardExpression= evalExSyntaxConverter.standardizeExpression(expression)
+        assertEquals(expected,standardExpression)
     }
 }

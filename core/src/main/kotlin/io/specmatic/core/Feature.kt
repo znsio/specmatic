c591670df786097fe05c87a7c96c03d10c458e3e
Sufiyan <StarKhan6368@gmail.com>
2025-02-12 13:01:01 +0530

    [WIP] resolve deferred patterns in toOpenApiSchema

--- core/src/main/kotlin/io/specmatic/core/Feature.kt
+++ core/src/main/kotlin/io/specmatic/core/Feature.kt
@@ -1629,6 +1629,8 @@ data class Feature(
             pattern is DateTimePattern || (pattern is DeferredPattern && pattern.pattern == "(datetime)") -> DateTimeSchema()
             pattern is DatePattern || (pattern is DeferredPattern && pattern.pattern == "(date)") -> DateSchema()
             pattern is UUIDPattern || (pattern is DeferredPattern && pattern.pattern == "(uuid)") -> UUIDSchema()
+            pattern is DatePattern || (pattern is DeferredPattern && pattern.pattern == "(date)") -> StringSchema()
+            pattern is UUIDPattern || (pattern is DeferredPattern && pattern.pattern == "(uuid)") -> StringSchema()
             pattern is StringPattern || pattern is EmptyStringPattern || (pattern is DeferredPattern && pattern.pattern == "(string)") || (pattern is DeferredPattern && pattern.pattern == "(nothing)") -> StringSchema()
             pattern is NullPattern || (pattern is DeferredPattern && pattern.pattern == "(null)") -> Schema<Any>().apply {
                 this.nullable = true

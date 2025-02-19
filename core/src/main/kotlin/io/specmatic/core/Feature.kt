80151ab53fcee0f81dc8e79d308cd699c88449cc
Sufiyan <StarKhan6368@gmail.com>
2025-02-20 04:15:31 +0530

    Fix Pattern to schema conversion
    - Regex goes into pattern not format
    - Take Number exclusiveValues if set.

--- core/src/main/kotlin/io/specmatic/core/Feature.kt
+++ core/src/main/kotlin/io/specmatic/core/Feature.kt
@@ -1584,15 +1584,15 @@ data class Feature(
                 schema.apply {
                     minimum = pattern.minimum;
                     maximum = pattern.maximum;
-                    exclusiveMinimum = pattern.exclusiveMinimum;
-                    exclusiveMaximum = pattern.exclusiveMaximum
+                    exclusiveMinimum = pattern.exclusiveMinimum.takeIf { it };
+                    exclusiveMaximum = pattern.exclusiveMaximum.takeIf { it }
                 }
             }
             pattern is StringPattern -> {
                 StringSchema().apply {
                     minLength = pattern.minLength;
                     maxLength = pattern.maxLength;
-                    format = pattern.regex
+                    this.pattern = pattern.regex
                 }
             }
             pattern is DictionaryPattern -> {
@@ -1684,8 +1684,6 @@ data class Feature(
             pattern is DateTimePattern || (pattern is DeferredPattern && pattern.pattern == "(datetime)") -> DateTimeSchema()
             pattern is DatePattern || (pattern is DeferredPattern && pattern.pattern == "(date)") -> DateSchema()
             pattern is UUIDPattern || (pattern is DeferredPattern && pattern.pattern == "(uuid)") -> UUIDSchema()
-            pattern is DatePattern || (pattern is DeferredPattern && pattern.pattern == "(date)") -> StringSchema()
-            pattern is UUIDPattern || (pattern is DeferredPattern && pattern.pattern == "(uuid)") -> StringSchema()
             pattern is StringPattern || pattern is EmptyStringPattern || (pattern is DeferredPattern && pattern.pattern == "(string)") || (pattern is DeferredPattern && pattern.pattern == "(nothing)") -> StringSchema()
             pattern is NullPattern || (pattern is DeferredPattern && pattern.pattern == "(null)") -> Schema<Any>().apply {
                 this.nullable = true

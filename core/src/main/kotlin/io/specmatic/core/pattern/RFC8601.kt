19346314616a5b1683a94adf2ef8ca8dcc053751
Joel Rosario <cirquitz@gmail.com>
2025-02-20 10:58:11 +0530

    Added tests for TimePattern, renamed RCF 8601 to ISO 8601

--- core/src/main/kotlin/io/specmatic/core/pattern/RFC8601.kt
+++ core/src/main/kotlin/io/specmatic/core/pattern/RFC8601.kt
@@ -4,7 +4,7 @@ import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.util.regex.Pattern
 
-private val RFC8601_PATTERN: Pattern = Pattern.compile (
+private val ISO8601_PATTERN: Pattern = Pattern.compile (
     """^(?<hour>2[0-3]|[01][0-9]):?(?<minute>[0-5][0-9]):?(?<second>[0-5][0-9])(?<timezone>Z|[+-](?:2[0-3]|[01][0-9])(?::?(?:[0-5][0-9]))?)?$"""
 )
 
@@ -13,9 +13,9 @@ class RFC8601 {
         private val timeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
 
         fun parse(dateTime: String) {
-            val matcher = RFC8601_PATTERN.matcher(dateTime)
+            val matcher = ISO8601_PATTERN.matcher(dateTime)
             if(!matcher.matches()) {
-                throw ContractException("Error while parsing the time as per RFC 8601: $dateTime")
+                throw ContractException("Error while parsing the time as per ISO 8601: $dateTime")
             }
         }
 
--- core/src/test/kotlin/io/specmatic/core/pattern/TimePatternTest.kt
+++ core/src/test/kotlin/io/specmatic/core/pattern/TimePatternTest.kt
@@ -6,6 +6,8 @@ import io.specmatic.core.value.StringValue
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class TimePatternTest {
     @Test
@@ -49,5 +51,27 @@ class TimePatternTest {
             val match = TimePattern.matches(time, Resolver())
             assertThat(match).isInstanceOf(Result.Success::class.java)
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "01:01:01, true",
+        "a23:59:59, false",
+        "235959, true",
+        "01:01:01Z, true",
+        "010101Z, true",
+        "01:01:01+05:30, true",
+        "01:01:01+05:30d, false",
+        "010101+0530, true",
+        "010101d+0530, false",
+        "010101+0530d, false",
+        "01:01:01-01:00, true",
+        "01:01:01b-01:00a, false",
+        "010101-0100, true",
+        "010101c-0100d, false",
+    )
+    fun `RFC 6801 regex should validate time`(time: String, isValid: Boolean) {
+        val result = TimePattern.matches(StringValue(time), Resolver())
+        assertEquals(isValid, result is Result.Success)
     }
 }

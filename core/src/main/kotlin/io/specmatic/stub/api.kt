581623db27a0d9888cb772faa106a69ecb65530c
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2025-03-04 17:54:35 +0530

    Update withAbsolutePaths of dataDirPaths

--- core/src/main/kotlin/io/specmatic/stub/api.kt
+++ core/src/main/kotlin/io/specmatic/stub/api.kt
@@ -364,7 +364,7 @@ fun loadContractStubsFromFiles(
         dataDirFiles(dataDirPaths)
     )
 
-    logger.debug("Scanning for stub expectations from the following example directories: '${dataDirPaths.withAbsolutePaths()}'")
+    logger.debug("Scanning for stub expectations from the following example directories:${System.lineSeparator()} '${dataDirPaths.withAbsolutePaths()}'")
     return loadImplicitExpectationsFromDataDirsForFeature(
         features,
         dataDirPaths,
@@ -411,7 +411,7 @@ fun loadExpectationsForFeatures(
 }
 
 private fun List<String>.withAbsolutePaths(): String {
-    return this.joinToString(", ") { "$it (absolute path ${File(it).canonicalPath})" }
+    return this.joinToString("${System.lineSeparator()}-") { "$it (absolute path ${File(it).canonicalPath})" }.prependIndent("  ")
 }
 
 private fun  List<Pair<String, Feature>>.overrideInlineExamplesWithSameNameFrom(dataFiles: List<File>): List<Pair<String, Feature>> {

9f052ddddf1d2be3ca5431bedd54b7e4311f51fc
Joel Rosario <cirquitz@gmail.com>
2025-04-01 19:39:50 +0530

    Refactored the lifecycle hook into a SAM interface

--- core/src/main/kotlin/io/specmatic/core/lifecycle/AfterLoadingStaticExamples.kt
+++ core/src/main/kotlin/io/specmatic/core/lifecycle/AfterLoadingStaticExamples.kt
@@ -3,6 +3,6 @@ package io.specmatic.core.lifecycle
 import io.specmatic.core.Feature
 import io.specmatic.mock.ScenarioStub
 
-interface AfterLoadingStaticExamples {
+fun interface AfterLoadingStaticExamples {
     fun call(examplesUsedFor: ExamplesUsedFor, examples: List<Pair<Feature, List<ScenarioStub>>>)
 }
--- core/src/main/kotlin/io/specmatic/core/lifecycle/LifecycleHooks.kt
+++ core/src/main/kotlin/io/specmatic/core/lifecycle/LifecycleHooks.kt
@@ -1,12 +1,7 @@
 package io.specmatic.core.lifecycle
 
-import io.specmatic.core.Feature
-import io.specmatic.mock.ScenarioStub
-
 object LifecycleHooks {
-    var afterLoadingStaticExamples: AfterLoadingStaticExamples = object : AfterLoadingStaticExamples {
-        override fun call(examplesUsedFor: ExamplesUsedFor, examples: List<Pair<Feature, List<ScenarioStub>>>) {
+    var afterLoadingStaticExamples: AfterLoadingStaticExamples = AfterLoadingStaticExamples { _, _ ->
             // No-op
         }
-    }
 }

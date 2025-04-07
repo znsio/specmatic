342b3a8100063f68d29d2a160345cc919b73b7de
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2025-04-07 15:14:22 +0530

    Use the new plugin and workflow

---
+++ build.gradle.kts
@@ -0,0 +1,104 @@
+plugins {
+    id("io.specmatic.gradle")
+}
+
+allprojects {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
+specmatic {
+    publishToMavenCentral()
+    publishTo("specmaticPrivate", "https://maven.pkg.github.com/znsio/specmatic-private-maven-repo")
+    withOSSLibrary(project(":core")) {
+        githubRelease()
+        publish {
+            pom {
+                name = "Specmatic"
+                description =
+                    "Turn your contracts into executable specifications. Contract Driven Development - Collaboratively Design & Independently Deploy MicroServices & MicroFrontends."
+                url = "https://specmatic.io"
+                licenses {
+                    license {
+                        name = "MIT"
+                        url = "https://github.com/znsio/specmatic/blob/main/License.md"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "specmaticBuilders"
+                        name = "Specmatic Builders"
+                        email = "info@specmatic.io"
+                    }
+                }
+                scm {
+                    connection = "https://github.com/znsio/specmatic"
+                    url = "https://specmatic.io/"
+                }
+            }
+        }
+    }
+    withOSSLibrary(project(":junit5-support")) {
+        githubRelease()
+        publish {
+            pom {
+                name = "SpecmaticJUnit5Support"
+                description = "Specmatic JUnit 5 Support"
+                url = "https://specmatic.io"
+                licenses {
+                    license {
+                        name = "MIT"
+                        url = "https://github.com/znsio/specmatic/blob/main/License.md"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "specmaticBuilders"
+                        name = "Specmatic Builders"
+                        email = "info@specmatic.io"
+                    }
+                }
+                scm {
+                    connection = "https://github.com/znsio/specmatic"
+                    url = "https://specmatic.io/"
+                }
+            }
+        }
+    }
+
+    withOSSApplicationLibrary(project(":specmatic-executable")) {
+        mainClass = "application.SpecmaticApplication"
+        githubRelease()
+        dockerBuild()
+        publish {
+            pom {
+                name = "Specmatic Executable"
+                description = "Command-line standalone executable jar for Specmatic"
+                url = "https://specmatic.io"
+                licenses {
+                    license {
+                        name = "MIT"
+                        url = "https://github.com/znsio/specmatic/blob/main/License.md"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "specmaticBuilders"
+                        name = "Specmatic Builders"
+                        email = "info@specmatic.io"
+                    }
+                }
+                scm {
+                    connection = "https://github.com/znsio/specmatic"
+                    url = "https://specmatic.io/"
+                }
+            }
+        }
+    }
+}
+tasks.getByName("beforeReleaseBuild").dependsOn("check")
+tasks.getByName("afterReleaseBuild").dependsOn("publishAllPublicationsToSpecmaticPrivateRepository")
+tasks.getByName("afterReleaseBuild").dependsOn("publishAllPublicationsToMavenCentralRepository")
+tasks.getByName("afterReleaseBuild").dependsOn("dockerBuildxPublish")

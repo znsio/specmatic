cef564c4a8dac365fbb31d8b27efa6d73c5b8902
Sufiyan <StarKhan6368@gmail.com>
2025-04-14 16:37:38 +0530

    Remove ToOpenApi Command

--- application/src/main/kotlin/application/SpecmaticCommand.kt
+++ application/src/main/kotlin/application/SpecmaticCommand.kt
@@ -21,7 +21,6 @@ import java.util.concurrent.Callable
             GenerateCompletion::class,
             GraphCommand::class,
             MergeCommand::class,
-            ToOpenAPICommand::class,
             ImportCommand::class,
             ProxyCommand::class,
             ExamplesCommand::class,

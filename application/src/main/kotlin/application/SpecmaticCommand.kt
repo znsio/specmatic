87d3deec42c36a6cdbc57ae446dd5a329e4c0bb1
Sufiyan <StarKhan6368@gmail.com>
2025-04-14 16:40:23 +0530

    Remove Graph Command

--- application/src/main/kotlin/application/SpecmaticCommand.kt
+++ application/src/main/kotlin/application/SpecmaticCommand.kt
@@ -19,7 +19,6 @@ import java.util.concurrent.Callable
             CompatibleCommand::class,
             DifferenceCommand::class,
             GenerateCompletion::class,
-            GraphCommand::class,
             ImportCommand::class,
             ProxyCommand::class,
             ExamplesCommand::class,

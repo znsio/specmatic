e3c422b08404bcec9c833dbe6150806d9b51677a
Kaushal Vora <vorakaushal1988@gmail.com>
2025-04-17 14:28:25 +0530

    Added test with stub for testing email pattern

--- core/src/test/kotlin/io/specmatic/core/pattern/EmailPatternTest.kt
+++ core/src/test/kotlin/io/specmatic/core/pattern/EmailPatternTest.kt
@@ -1,12 +1,19 @@
 package io.specmatic.core.pattern
 
 import io.specmatic.GENERATION
+import io.specmatic.conversions.OpenApiSpecification
+import io.specmatic.core.EXAMPLES_DIR_SUFFIX
+import io.specmatic.core.HttpRequest
 import io.specmatic.core.Resolver
 import io.specmatic.core.Result
+import io.specmatic.core.value.JSONObjectValue
 import io.specmatic.core.value.StringValue
+import io.specmatic.mock.ScenarioStub
+import io.specmatic.stub.HttpStub
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import java.io.File
 
 class EmailPatternTest {
     @Test
@@ -38,5 +45,36 @@ class EmailPatternTest {
         val matchResult = pattern.matches(filledInValue, resolver)
 
         assertThat(matchResult.isSuccess()).withFailMessage(matchResult.reportString()).isTrue()
+    }
+
+    private fun File.getExternalExamplesFromContract(): List<ScenarioStub> {
+        val attributeExamples = this.parentFile.resolve("${this.nameWithoutExtension}$EXAMPLES_DIR_SUFFIX")
+        val normalExamples = this.parentFile.resolve("${this.nameWithoutExtension}${EXAMPLES_DIR_SUFFIX}_no_attr")
+
+        return attributeExamples.listFiles()?.map { ScenarioStub.readFromFile(it) }?.plus(
+            normalExamples.listFiles()?.map { ScenarioStub.readFromFile(it) } ?: emptyList()
+        ) ?: emptyList()
+    }
+
+    @Test
+    fun `should handle email format`() {
+        val specFilepath = File("src/test/resources/openapi/spec_with_format_email_with_external_example/spec.yaml")
+
+        val feature = OpenApiSpecification.fromFile(specFilepath.absolutePath).toFeature()
+        val stubScenarios = specFilepath.getExternalExamplesFromContract()
+
+        HttpStub(feature, stubScenarios).use {
+            val response = it.client.execute(
+                HttpRequest(
+                "GET",
+                "/pets/1"
+                )
+            )
+
+            assertThat(response.status).isEqualTo(200)
+
+            val body = (response.body as JSONObjectValue).jsonObject
+            assertThat(body.keys).containsExactlyInAnyOrder("id", "name", "type", "status", "email")
+        }
     }
 }

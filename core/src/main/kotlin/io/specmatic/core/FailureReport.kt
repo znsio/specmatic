25cf0877a7cd41e3fbc6215b42b615781f2b7a88
Joel Rosario <cirquitz@gmail.com>
2025-05-13 13:53:15 +0530

    Refactored FailureReport.matchFailureDetails

--- core/src/main/kotlin/io/specmatic/core/FailureReport.kt
+++ core/src/main/kotlin/io/specmatic/core/FailureReport.kt
@@ -41,13 +41,13 @@ data class FailureReport(val contractPath: String?, private val scenarioMessage:
     }
 
     private fun matchFailureDetails(matchFailureDetails: MatchFailureDetails): String {
-        return matchFailureDetails.let { (breadCrumbs, errorMessages) ->
-            val breadCrumbString = startOfBreadCrumbPrefix(breadCrumbString(breadCrumbs))
+        val (breadCrumbs, errorMessages) = matchFailureDetails
 
-            val errorMessagesString = errorMessagesToString(errorMessages)
+        val breadCrumbString = startOfBreadCrumbPrefix(breadCrumbString(breadCrumbs))
 
-            listOf(breadCrumbString, errorMessagesString.prependIndent("   ")).filter { it.isNotBlank() }.joinToString("${System.lineSeparator()}${System.lineSeparator()}")
-        }
+        val errorMessagesString = errorMessagesToString(errorMessages)
+
+        return listOf(breadCrumbString, errorMessagesString.prependIndent("   ")).filter { it.isNotBlank() }.joinToString("${System.lineSeparator()}${System.lineSeparator()}")
     }
 
     private fun errorMessagesToString(errorMessages: List<String>) =

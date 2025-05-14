99a0eef4674ba14ac5d452506a9914b7042ab065
Sufiyan <StarKhan6368@gmail.com>
2025-05-14 17:07:40 +0530

    Tests for serialization and de-ser for Doubles

--- core/src/test/kotlin/io/specmatic/core/utilities/JSONSerialisationKtTest.kt
+++ core/src/test/kotlin/io/specmatic/core/utilities/JSONSerialisationKtTest.kt
@@ -1,13 +1,48 @@
 package io.specmatic.core.utilities
 
 import io.specmatic.core.pattern.parsedJSONObject
+import io.specmatic.core.value.NumberValue
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.math.BigDecimal
 
 class JSONSerialisationKtTest {
     @Test
     fun `should parse a JSON string with a UTF-8 BOM`() {
         val jsonContent = "\uFEFF{\"greeting\":\"hello\"}"
         assertDoesNotThrow { parsedJSONObject(jsonContent) }
+    }
+
+    @ParameterizedTest
+    @MethodSource("floatingPointValues")
+    fun `should be able to de-serialise floating point values from json to number value`(value: Number) {
+        val jsonString = """{"value": $value}"""
+        val valueMap = jsonStringToValueMap(jsonString)
+        val parsedValue = (valueMap["value"] as NumberValue).nativeValue
+
+        assertThat(
+            BigDecimal(parsedValue.toString())
+        ).isEqualTo(
+            BigDecimal(value.toString())
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("floatingPointValues")
+    fun `should be able to serialise floating point values from json to number value`(value: Number) {
+        val valueMap = mapOf("value" to NumberValue(value))
+        val jsonContent = valueMapToUnindentedJsonString(valueMap)
+
+        assertThat(jsonContent).isEqualTo("""{"value":$value}""")
+    }
+
+    companion object {
+        @JvmStatic
+        fun floatingPointValues(): List<Number> {
+            return listOf(Float.MIN_VALUE, Float.MAX_VALUE, Double.MIN_VALUE, Double.MAX_VALUE)
+        }
     }
 }

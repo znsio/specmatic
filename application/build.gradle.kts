278883154ecb452ddd3e80d7c8efb7336f66797b
Specmatic Renovate <github-service-account@specmatic.io>
2025-05-27 14:00:31 +0000

    fix(deps): update dependency io.swagger.parser.v3:swagger-parser to v2.1.29

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation(project(":junit5-support"))
 
     implementation("io.ktor:ktor-client-cio:2.3.13")
-    implementation("io.swagger.parser.v3:swagger-parser:2.1.27") {
+    implementation("io.swagger.parser.v3:swagger-parser:2.1.29") {
         exclude(group = "org.mozilla", module = "rhino")
     }
     implementation("org.mozilla:rhino:1.7.15")

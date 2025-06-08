e4a4a0afe3a12ebe606aeb09e90ca5411d10e77d
Joel Rosario <cirquitz@gmail.com>
2025-06-08 19:42:23 +0530

    Watch child directories of _example directories

--- core/src/main/kotlin/io/specmatic/core/utilities/FileSystemChanges.kt
+++ core/src/main/kotlin/io/specmatic/core/utilities/FileSystemChanges.kt
@@ -3,11 +3,13 @@ package io.specmatic.core.utilities
 import io.specmatic.core.CONTRACT_EXTENSIONS
 import io.specmatic.core.EXAMPLES_DIR_SUFFIX
 import io.specmatic.core.JSON
+import io.specmatic.core.log.logger
 import java.io.File
+import java.nio.file.Path
 import java.nio.file.WatchKey
 
 class FileSystemChanges(
-    key: WatchKey,
+    private val key: WatchKey,
 ) {
     private val rawEvents = key.pollEvents()
     private val events =
@@ -27,11 +29,22 @@ class FileSystemChanges(
         try {
             val file = File(event)
             val extension = file.extension.lowercase()
-            return extension in CONTRACT_EXTENSIONS || extension == JSON || file.nameWithoutExtension.endsWith(
-                EXAMPLES_DIR_SUFFIX
-            )
+
+            return extension in CONTRACT_EXTENSIONS ||
+                extension == JSON ||
+                file.nameWithoutExtension.endsWith(EXAMPLES_DIR_SUFFIX) ||
+                parentDirectoryIsExamplesDir(key.watchable() as? Path)
         } catch (e: Throwable) {
             return false
         }
+    }
+
+    private fun parentDirectoryIsExamplesDir(path: Path?): Boolean {
+        if (path == null) return false
+
+        logger.debug("Checking if $path is or is in an examples directory")
+
+        return path.toString().endsWith(EXAMPLES_DIR_SUFFIX) ||
+            path.toString().contains(EXAMPLES_DIR_SUFFIX + File.separator)
     }
 }
